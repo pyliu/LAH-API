71769b0583d268a39853599562a116e57e218f4c
pyliu <0541.b@mail.tyland.gov.tw>
2019-07-12 16:45:53 +0800

    refactoring: move outputCSV to proper subclass

--- www/include/FileAPISQLCsvCommand.class.php
+++ www/include/FileAPISQLCsvCommand.class.php
@@ -29,6 +29,34 @@ class FileAPISQLCsvCommand extends FileAPICommand {
 
     function __destruct() {}
 
+    private function outputCSV($data, $skip_header = false) {
+        header("Content-Type: text/csv");
+        $out = fopen("php://output", 'w'); 
+        if (is_array($data)) {
+            $firstline_flag = false;
+            foreach ($data as $row) {
+                if (!$skip_header && !$firstline_flag) {
+                    if (!is_array($row)) {
+                        fputcsv($out, array_values(array(iconv("utf-8", "big5", "錯誤說明"))), ',', '"');
+                        fputcsv($out, array_values(array(iconv("utf-8", "big5", "第一列的資料非陣列，無法轉換為CSV檔案"))), ',', '"');
+                        break;
+                    }
+                    $firstline = array_map(array($this, "mapColumns"), array_keys($row));
+                    //$firstline = array_map("self::mapColumns", array_keys($row));
+                    fputcsv($out, $firstline, ',', '"');
+                    $firstline_flag = true;
+                }
+                array_walk($row, array($this, "cleanData"));
+                //array_walk($row, "self::cleanData");
+                fputcsv($out, array_values($row), ',', '"');
+            }
+        } else {
+            fputcsv($out, array_values(array(iconv("utf-8", "big5", "錯誤說明"))), ',', '"');
+            fputcsv($out, array_values(array(iconv("utf-8", "big5", "傳入之參數非陣列格式無法匯出！"))), ',', '"');
+        }
+        fclose($out);
+    }
+
     public function execute() {
         $q = new Query();
         $data = $q->getSelectSQLData($this->sql);
