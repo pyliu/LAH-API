10d3321a425c6aac4c32ae891f23137b90927c47
pyliu <0541.b@mail.tyland.gov.tw>
2019-07-12 19:01:30 +0800

    add AI00601 report

---
+++ www/assets/files/txt_AI00601_E.sql
@@ -0,0 +1,11 @@
+select a.MM00 || a.MM48 || a.MM49 || LPAD(a.MM01, 4, '0') ||
+       LPAD(b.EE09, 10, ' ') || LPAD(a.MM13, 10, ' ') ||
+       RPAD(c.LNAM, 60, ' ') || RPAD(c.LADR, 60, ' ') AS AI00601_E
+  from SRMNGR a, SREBOW b, SRLNID c
+ where --(a.MM48 || a.MM49 between '036200000000' and '036399999999')
+   a.MM48 in ('0362', '0363')
+   and (a.MM00 = 'E')
+   and (b.ED48 = a.MM48)
+   and (b.ED49 = a.MM49)
+   and (b.EE01 = a.MM01)
+   and (c.LIDN = a.MM13)
