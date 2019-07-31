f19755b1b87cf5ebc54271339c5349ca456b35b7
pyliu <0541.b@mail.tyland.gov.tw>
2019-07-31 17:02:44 +0800

    sync

--- www/debug.php
+++ www/debug.php
@@ -3,9 +3,14 @@ require_once("./include/init.php");
 require_once("./include/Query.class.php");
 require_once("./include/MSDB.class.php");
 
-//$query = new Query();
-//var_dump($query->getPrcCaseAll("108HDB1011970"));
-//var_dump($query->getProblematicCrossCases());
 $ms_db = new MSDB();
-var_dump($ms_db->isConnected());
+var_dump(print_r($ms_db->fetchAll("SELECT TOP 10 * FROM dbo.Message")));
+
+/*
+$sock = socket_create(AF_INET, SOCK_DGRAM, SOL_UDP);
+$msg = "This is a test";
+$len = strlen($msg);
+echo socket_sendto($sock, $msg, $len, 0, '220.1.35.48', 12345);
+socket_close($sock);
+*/
 ?>
