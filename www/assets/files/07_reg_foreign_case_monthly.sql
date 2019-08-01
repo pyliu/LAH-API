d3824faad6b7fab46cf4cca15eec121844ec4905
pyliu <0541.b@mail.tyland.gov.tw>
2019-08-01 11:34:31 +0800

    update SQL

--- www/assets/files/07_reg_foreign_case_monthly.sql
+++ www/assets/files/07_reg_foreign_case_monthly.sql
@@ -1,47 +1,17 @@
-SELECT SQ.RM01   AS "收件年", -- 權利人&義務人為外國人案件
-       SQ.RM02   AS "收件字",
-       SQ.RM03   AS "收件號",
-       SQ.RM09   AS "登記原因代碼",
-       k.KCNT    AS "登記原因",
-       SQ.RM07_1 AS "收件日期",
-       SQ.RM58_1 AS "結案日期",
-       SQ.RM18   AS "權利人統一編號",
-       SQ.RM19   AS "權利人姓名",
-       SQ.RM21   AS "義務人統一編號",
-       SQ.RM22   AS "義務人姓名",
-       (CASE
-          WHEN SQ.RM30 = 'A' THEN '初審'
-          WHEN SQ.RM30 = 'B' THEN '複審'
-          WHEN SQ.RM30 = 'H' THEN '公告'
-          WHEN SQ.RM30 = 'I' THEN '補正'
-          WHEN SQ.RM30 = 'R' THEN '登錄'
-          WHEN SQ.RM30 = 'C' THEN '校對'
-          WHEN SQ.RM30 = 'U' THEN '異動完成'
-          WHEN SQ.RM30 = 'F' THEN '結案'
-          WHEN SQ.RM30 = 'X' THEN '補正初核'
-          WHEN SQ.RM30 = 'Y' THEN '駁回初核'
-          WHEN SQ.RM30 = 'J' THEN '撤回初核'
-          WHEN SQ.RM30 = 'K' THEN '撤回'
-          WHEN SQ.RM30 = 'Z' THEN '歸檔'
-          WHEN SQ.RM30 = 'N' THEN '駁回'
-          WHEN SQ.RM30 = 'L' THEN '公告初核'
-          WHEN SQ.RM30 = 'E' THEN '請示'
-          WHEN SQ.RM30 = 'D' THEN '展期'
-          ELSE SQ.RM30
-      END) AS "辦理情形",
-       (CASE
-          WHEN SQ.RM31 = 'A' THEN '結案'
-          WHEN SQ.RM31 = 'B' THEN '撤回'
-          WHEN SQ.RM31 = 'C' THEN '併案'
-          WHEN SQ.RM31 = 'D' THEN '駁回'
-          WHEN SQ.RM31 = 'E' THEN '請示'
-          ELSE SQ.RM31
-      END) AS "結案與否"
-  FROM (SELECT *
-    FROM MOICAD.RLNID p, MOICAS.CRSMS tt
-    WHERE tt.RM07_1 LIKE '10807%'
-      AND p.LCDE In ('2', '8', 'C', 'D') -- 代碼檔 09
-      AND (tt.RM18 = p.LIDN OR tt.RM21 = p.LIDN)
-  ) SQ
-  LEFT JOIN MOICAD.RKEYN k ON k.KCDE_2 = SQ.RM09
- WHERE k.KCDE_1 = '06' AND SQ.RM09 in ('64', '65'); -- 買賣 64, 贈與 65
+select a.MM00 || a.MM48 || a.MM49 || LPAD(a.MM01, 4, '0') ||
+       LPAD(b.EE09, 10, ' ') || LPAD(a.MM13, 10, ' ') ||
+       RPAD(c.LNAM, 60, ' ') || RPAD(c.LADR, 60, ' ') AS AI00601_E
+  from SRMNGR a, SREBOW b, SRLNID c
+ where
+   a.MM48 in ('0362', '0363')
+    -- a.MM48 in ('0200', '0202', '0205', '0210') -- A21
+    -- a.MM48 in ('0255') -- 草漯
+    -- a.MM48 in ('0255', '0275', '0277', '0278', '0377') -- 草漯UNIT3
+    -- a.MM48 in ('0255', '0377', '0392') -- 草漯UNIT6
+    --(a.MM48 || a.MM49 between '031800000000' and '032299999999') -- 中壢運動公園
+   and (a.MM00 = 'E')
+   and (b.ED48 = a.MM48)
+   and (b.ED49 = a.MM49)
+   and (b.EE01 = a.MM01)
+   and (c.LIDN = a.MM13)
+ order by a.MM48, a.MM49
