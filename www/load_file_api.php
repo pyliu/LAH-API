3fbd07936eb95291e2a6df2e0f893fee46bfaee4
pyliu <0541.b@mail.tyland.gov.tw>
2019-09-16 16:04:13 +0800

    add log

--- www/load_file_api.php
+++ www/load_file_api.php
@@ -11,6 +11,7 @@ function echoErrorJSONString($msg = "", $status = STATUS_CODE::DEFAULT_FAIL) {
 
 switch ($_POST["type"]) {
     case "load_select_sql":
+        $log->info("XHR [load_select_sql] 查詢請求【".$_POST["file_name"]."】");
         $path = "./assets/files/".$_POST["file_name"];
         if (file_exists($path)) {
             $result = array(
@@ -18,12 +19,15 @@ switch ($_POST["type"]) {
                 "data" => file_get_contents($path),
                 "query_string" => "file_name=".$_POST["file_name"]."&type=".$_POST["type"]
             );
+            $log->info("XHR [load_select_sql] 讀取成功【".$path."】");
             echo json_encode($result, 0);
         } else {
+            $log->error("XHR [load_select_sql] 找不到檔案【".$path."】");
             echoErrorJSONString("找不到檔案【".$path."】");
         }
         break;
     default:
+        $log->error("XHR [".$_POST["type"]."] 不支援的讀取型態");
         echoErrorJSONString("不支援的讀取型態【".$_POST["type"]."】", STATUS_CODE::UNSUPPORT_FAIL);
         break;
 }
