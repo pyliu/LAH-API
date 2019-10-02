56fd1ed19df62e6b36a9910b8780c1a07fff4d55
pyliu <0541.b@mail.tyland.gov.tw>
2019-10-02 13:51:28 +0800

    fix $day bug

--- www/allcases.php
+++ www/allcases.php
@@ -2,6 +2,12 @@
 require_once("./include/init.php");
 require_once("./include/Query.class.php");
 
+$qday = $_REQUEST["date"];
+$qday = preg_replace("/\D+/", "", $qday);
+if (empty($qday) || !preg_match("/^[0-9]{7}$/i", $qday)) {
+  $qday = $today; // 今天
+}
+
 $query = new Query();
 $query->echoAllCasesHTML($qday);
 ?>
