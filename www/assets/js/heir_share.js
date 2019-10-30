0b65704d6d546c49d990a8ae6655337989279227
pyliu <3634168+pyliu@users.noreply.github.com>
2019-10-30 17:21:56 +0800

    heir share - add 家產繼承 calculation

--- www/assets/js/heir_share.js
+++ www/assets/js/heir_share.js
@@ -5,6 +5,91 @@ function isEmpty(variable) {
     return false;
 }
 
+let showModal = opts => {
+	let body = opts.body;
+	let title = opts.title;
+	let size = opts.size;	// sm, md, lg, xl
+	let callback = opts.callback;
+	if (isEmpty(title)) {
+		title = "... 請輸入指定標題 ...";
+	}
+	if (isEmpty(body)) {
+		body = "... 請輸入指定內文 ...";
+	}
+	if (isEmpty(size)) {
+		size = "md";
+	}
+	
+	let modal_element = $("#bs_modal_template");
+	
+	// Try to use Vue.js
+	window.modalApp.title = title;
+	window.modalApp.body = body;
+	window.modalApp.sizeClass = "modal-" + size;
+	window.modalApp.optsClass = opts.class || "";
+
+	if (typeof callback == "function") {
+		modal_element.one('shown.bs.modal', callback);
+	}
+
+	// backdrop: 'static' => not close by clicking outside dislog
+	modal_element.modal({backdrop: 'static'});
+}
+
+let closeModal = callback => {
+	if (typeof callback == "function") {
+		$("#bs_modal_template").one('hidden.bs.modal', callback);
+	}
+	$("#bs_modal_template").modal("hide");
+}
+
+let initModalUI = () => {
+	// add modal element to show the popup html message
+	if ($("#bs_modal_template").length == 0) {
+		$("body").append($.parseHTML(`
+			<div class="modal fade" id="bs_modal_template" tabindex="-1" role="dialog" aria-labelledby="bs_modal_template" aria-hidden="true">
+				<div class="modal-dialog modal-dialog-centered modal-dialog-scrollable" v-bind:class="sizeClass" role="document">
+					<div class="modal-content">
+						<com-header :in-title="title"></com-header>
+						<com-body :in-body="body" :in-opts-class="optsClass"></com-body>
+						<com-footer></com-footer>
+					</div>
+				</div>
+			</div>
+		`));
+		// Try to use Vue.js
+		window.modalApp = new Vue({
+			el: '#bs_modal_template',
+			data: {
+				body: 'Hello Vue!',
+				title: 'Hello Vue!',
+				sizeClass: 'modal-md',
+				optsClass: ''
+			},
+			components: {
+				"com-header": {
+					props: ["inTitle"],
+					template: `<div class="modal-header">
+						<h4 class="modal-title"><span v-html="inTitle"></span></h4>
+						<button type="button" class="close" data-dismiss="modal">&times;</button>
+					</div>`
+				},
+				"com-body": {
+					props: ["inOptsClass", "inBody"],
+					template: `<div class="modal-body" :class="inOptsClass">
+					<p><span v-html="inBody"></span></p>
+				</div>`
+				},
+				"com-footer": {
+					template: `<div class="modal-footer">
+						<button type="button" class="btn btn-light" data-dismiss="modal">關閉</button>
+					</div>`
+				}
+			}
+		});
+	}
+}
+
 // other custom scripts start here
 $(document).ready((e) => {
     window.vueApp = new Vue({
@@ -13,14 +98,17 @@ $(document).ready((e) => {
             wizard: {
                 s0: {
                     title: "步驟1，選擇事實發生區間",
+                    legend: "被繼承人死亡時間",
                     seen: true,
                     value: "",
                     is76after: false
                 },
                 s01: {  // 光復前
                     title: "步驟2，家產 OR 私產？",
+                    legend: "被繼承財產種類",
                     seen: false,
-                    value: ""
+                    value: "",
+                    public_count: 0
                 },
                 s02: {   // 光復後
                     title: "步驟2，輸入各項目人數",
@@ -28,6 +116,7 @@ $(document).ready((e) => {
                     value: ""
                 }
             },
+            heir_denominator: 1,
             prev_step: {},
             now_step: {},
             VueOK: true,
@@ -51,8 +140,30 @@ $(document).ready((e) => {
                 if (this.now_step !== this.wizard.s0 && this.prev_step.seen !== undefined) {
                     this.prev_step.seen = true;
                     this.now_step.seen = false;
+                    this.now_step.value = "";
                     this.now_step = this.prev_step;
                 } 
+            },
+            filter: function(e) {
+                let val = e.target.value.replace(/[^0-9]/g, "");
+                if (val == "" || val == "0" || val == 0) {
+                    val = 1;
+                }
+                let to = $(e.target).data("filter-to");
+                switch(to) {
+                    case "heir_denominator":
+                        this.heir_denominator = val;
+                        break;
+                    case "wizard.s01.public_count":
+                        this.wizard.s01.public_count = val;
+                        break;
+                    default:
+                        console.log(`to: ${to}??`);
+                        showModal({
+                            title: "錯誤訊息",
+                            body: `to: ${to}, val: ${val}`
+                        });
+                }
             },
             s0ValueSelected: function(e) {
                 switch(this.wizard.s0.value) {
@@ -89,6 +200,7 @@ $(document).ready((e) => {
         mounted: function() {  // like jQuery ready
             $("#VueOK").toggleClass("d-none");
             this.now_step = this.wizard.s0;
+            initModalUI();
         }
     });
 });
