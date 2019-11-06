76d5318251aed4dd97bc087f94502ead76f12d22
pyliu <3634168+pyliu@users.noreply.github.com>
2019-11-06 13:58:35 +0800

    refactoring cache.js

--- www/assets/js/cache.js
+++ www/assets/js/cache.js
@@ -2,14 +2,15 @@
 if (jQuery && localStorage) {
     // restore cached data from localStorage
     $(document).ready(function(e) {
-        for (var key in localStorage) {
+        let cached_el_selector = "input[type='text'], select, textarea";
+        for (let key in localStorage) {
             if (key == "length" || key == "key" || key == "getItem" || key == "setItem" || key == "removeItem" || key == "clear" || key == "" || key == undefined) {
                 continue;
             }
             let el = $("#"+key);
-            if (el.length > 0 && el.is("input[type='text'], select, textarea")) {
+            if (el.length > 0 && el.is(cached_el_selector)) {
                 console.log(`Found #${key}! Set element value to ${localStorage[key]}`);
-                $("#"+key).val(localStorage[key]);
+                el.val(localStorage[key]);
             } else {
                 // remove non input type cached data
                 localStorage.removeItem(key);
@@ -17,10 +18,10 @@ if (jQuery && localStorage) {
         }
 
         // for cache purpose
-        var cacheIt = function(el) {
-            var this_text_input = $(el);
-            var val = this_text_input.val();
-            var ele_id = this_text_input.attr("id");
+        let cacheIt = function(el) {
+            let this_text_input = $(el);
+            let val = this_text_input.val();
+            let ele_id = this_text_input.attr("id");
             if (val === undefined || $.trim(val) == "") {
                 localStorage.removeItem(ele_id);
             } else {
@@ -28,19 +29,19 @@ if (jQuery && localStorage) {
             }
         }
         window.pyliuCacheTimer = setInterval(function(e) {
-            $("input[type='text'], select, textarea").each(function(index, el) {
+            $(cached_el_selector).each(function(index, el) {
                 if (!$(el).hasClass("no-cache")) {
                     cacheIt(el);
                 }
             });
         }, 10000);
-        $("input[type='text'], select, textarea").on("blur", function(e) {
+        $(cached_el_selector).on("blur", function(e) {
             if (!$(e.target).hasClass("no-cache")) {
                 cacheIt(e.target);
             }
         });
     });
 } else {
-    alert("jQuery and localStorage are required to use cache.js!");
+    console.error("jQuery and localStorage are required to use cache.js!");
 }
 //]]>
