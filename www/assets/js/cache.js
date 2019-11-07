033b2721c46b3da33ccb2b22d2b923b7ee8bd543
pyliu <3634168+pyliu@users.noreply.github.com>
2019-11-07 16:49:09 +0800

    fix cache remove bug

--- www/assets/js/cache.js
+++ www/assets/js/cache.js
@@ -1,8 +1,23 @@
 //<![CDATA[
 if (jQuery && localStorage) {
+    let isJSON = function(str) {
+        if (typeof str == "string") {
+            try {
+                let obj = JSON.parse(str);
+                if (str.indexOf('{') > -1) {
+                    return true;
+                } else {
+                    return false;
+                }
+            } catch (e) {
+                return false;
+            }
+        }
+        return false;
+    }
     // restore cached data from localStorage
     $(document).ready(function(e) {
-        let cached_el_selector = "input[type='text'], select, textarea";
+        let cached_el_selector = "input[type='text'], input[type='number'], select, textarea";
         for (let key in localStorage) {
             if (key == "length" || key == "key" || key == "getItem" || key == "setItem" || key == "removeItem" || key == "clear" || key == "" || key == undefined) {
                 continue;
@@ -11,9 +26,11 @@ if (jQuery && localStorage) {
             if (el.length > 0 && el.is(cached_el_selector)) {
                 console.log(`Found #${key}! Set element value to ${localStorage[key]}`);
                 el.val(localStorage[key]);
-            } else {
-                // remove non input type cached data
+            }
+            if (localStorage[key] && isJSON(localStorage[key])) {
+                // remove json type cached data
                 localStorage.removeItem(key);
+                console.log(`Removed JSON object cached data in ${key}`);
             }
         }
 
