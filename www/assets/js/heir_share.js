cd47e7d6e7b79a0e2943754d149e18222650bb17
pyliu <3634168+pyliu@users.noreply.github.com>
2019-11-08 16:52:00 +0800

    comment wording

--- www/assets/js/heir_share.js
+++ www/assets/js/heir_share.js
@@ -133,6 +133,9 @@ $(document).ready((e) => {
             VueOK: true,
             debug: true
         },
+        computed: {
+
+        },
         components: {
             "counter-input": {
                 props: [ "max", "min" ],
