508de5ae55968a38f2f68093993043d25dbb95e5
pyliu <3634168+pyliu@users.noreply.github.com>
2019-12-20 10:35:20 +0800

    use global showModal to bring up note info

--- www/assets/js/components/xcase-check.js
+++ www/assets/js/components/xcase-check.js
@@ -49,42 +49,36 @@ if (Vue) {
         template: `<fieldset>
             <legend>跨所註記遺失檢測<small>(一周內)</small></legend>
             <button @click="check" class="btn btn-sm btn-outline-primary" data-toggle='tooltip'>立即檢測</button>
-            <button @click="show = true" class="btn btn-sm btn-outline-success">備註</button>
-            <b-modal
-                v-model="show"
-                size="lg"
-                scrollable
-                centered
-                hide-footer
-                no-close-on-backdrop
-                content-class="shadow"
-            >
-                <template v-slot:modal-title>跨所註記遺失檢測 小幫手提示</template>
-                <div class="d-block">
-                    <h5><span class="text-danger">※</span>通常發生的情況是案件內的權利人/義務人/代理人姓名內有罕字造成。</h5>
-                    <p class="text-info">QUERY:</p>
-                    &emsp;SELECT * <br />
-                    &emsp;FROM SCRSMS <br />
-                    &emsp;WHERE  <br />
-                    &emsp;&emsp;RM07_1 >= '1080715' <br />
-                    &emsp;&emsp;AND RM02 LIKE 'H%1' <br />
-                    &emsp;&emsp;AND (RM99 is NULL OR RM100 is NULL OR RM100_1 is NULL OR RM101 is NULL OR RM101_1 is NULL) 
-                    <br /><br />
-                    <p class="text-success">FIX:</p>
-                    &emsp;UPDATE MOICAS.CRSMS SET <br />
-                    &emsp;&emsp;RM99 = 'Y', <br />
-                    &emsp;&emsp;RM100 = '資料管轄所代碼', <br />
-                    &emsp;&emsp;RM100_1 = '資料管轄所縣市代碼', <br />
-                    &emsp;&emsp;RM101 = '收件所代碼', RM101_1 = '收件所縣市代碼' <br />
-                    &emsp;WHERE RM01 = '收件年' AND RM02 = '收件字' AND RM03 = '收件號'
-                </div>
-            </b-modal>
+            <button @click="showNote" class="btn btn-sm btn-outline-success">備註</button>
         </fieldset>`,
-        data: () => {
-            return { show: false }
-        },
         methods: {
+            showNote: function(e) {
+                showModal({
+                    title: "跨所註記遺失檢測 小幫手提示",
+                    body: `<div class="d-block">
+                        <h5><span class="text-danger">※</span>通常發生的情況是案件內的權利人/義務人/代理人姓名內有罕字造成。</h5>
+                        <p class="text-info">QUERY:</p>
+                        &emsp;SELECT * <br />
+                        &emsp;FROM SCRSMS <br />
+                        &emsp;WHERE  <br />
+                        &emsp;&emsp;RM07_1 >= '1080715' <br />
+                        &emsp;&emsp;AND RM02 LIKE 'H%1' <br />
+                        &emsp;&emsp;AND (RM99 is NULL OR RM100 is NULL OR RM100_1 is NULL OR RM101 is NULL OR RM101_1 is NULL) 
+                        <br /><br />
+                        <p class="text-success">FIX:</p>
+                        &emsp;UPDATE MOICAS.CRSMS SET <br />
+                        &emsp;&emsp;RM99 = 'Y', <br />
+                        &emsp;&emsp;RM100 = '資料管轄所代碼', <br />
+                        &emsp;&emsp;RM100_1 = '資料管轄所縣市代碼', <br />
+                        &emsp;&emsp;RM101 = '收件所代碼', RM101_1 = '收件所縣市代碼' <br />
+                        &emsp;WHERE RM01 = '收件年' AND RM02 = '收件字' AND RM03 = '收件號'
+                    </div>`,
+                    size: "lg"
+                });
+            },
             check: function(e) {
+                const h = this.$createElement;
+
                 toggle(e.target);
 	
                 let body = new FormData();
