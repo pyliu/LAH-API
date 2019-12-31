ec6cd08b24bba6cfd33db63502d75cc455339f1e
pyliu <3634168+pyliu@users.noreply.github.com>
2019-12-31 19:13:07 +0800

    wip

--- www/assets/js/components/expaa-mgt.js
+++ www/assets/js/components/expaa-mgt.js
@@ -5,47 +5,59 @@ if (Vue) {
             <b-container :class="['form-row']" fluid>
                 <div class="input-group input-group-sm col">
                     <div class="input-group-prepend">
-                    <span class="input-group-text" id="inputGroup-fee_query_date">日期</span>
+                        <span class="input-group-text" id="inputGroup-fee_query_date">日期</span>
                     </div>
                     <b-form-input
                         v-model="date"
                         id="fee_query_date"
                         placeholder="民國年月日"
-                        autocomplete="false"
                         :state="isDateValid"
-                        :class="['form-control', 'no-cache', 'border']"
+                        :class="['no-cache', 'border', 'bg-light', 'text-center']"
                         size="sm"
                         plaintext
-                        lazy-formatter
-                        :formatter="format"
                     >
                     </b-form-input>
-                    <button id="fee_query_date_button" class="btn btn-sm btn-outline-primary">查詢</button>
-                </div>
-                <div class="input-group input-group-sm col">
-                    <div class="input-group-prepend">
-                    <span class="input-group-text" id="inputGroup-expaa_query_number">給號</span>
+                    <div class="input-group-prepend ml-1">
+                        <span class="input-group-text" id="inputGroup-fee_query_number">電腦給號</span>
                     </div>
-                    <input type="number" max="9999999" min="1" id="expaa_query_number" name="expaa_query_number" class="form-control" placeholder="0006574" data-toggle="popover" data-content="需輸入7位數電腦給號，如「0021131」。" data-placement="bottom" />
-                    <button id="expaa_query_num_button" class="btn btn-sm btn-outline-secondary" title="針對電腦給號查詢">查詢</button>
-                </div>
-                <div class="filter-btn-group col">
-                    <!-- <button id="fee_query_date_button" class="btn btn-sm btn-outline-primary">查詢</button> -->
+                    <b-form-input
+                        v-model="number"
+                        id="fee_query_number"
+                        type="number"
+                        placeholder="七碼電腦給號"
+                        :state="isNumberValid"
+                        :formatter="format"
+                        size="sm"
+                    >
+                    </b-form-input>
+                    &ensp;
+                    <button id="fee_query_button" class="btn btn-sm btn-outline-primary">查詢</button>
+                    &ensp;
                     <button id="expaa_add_obsolete_button" class="btn btn-sm btn-outline-danger" title="新增作廢假資料以利空白規費單作廢">作廢</button>
-                    <button @click="popup" class="btn btn-sm btn-outline-success">備註</button>
+                    &ensp;
+                    <button @click="popup" class="btn btn-sm btn-outline-success">備註</button>&ensp;
                 </div>
             </b-container>
         </fieldset>`,
         data: () => {
             return {
-                date: "1081231"
+                date: "1081231",
+                number: ""
             }
         },
         computed: {
-            isDateValid: () => {
+            isDateValid: function() {
                 if (this.date == '' || this.date == undefined) {
                     return null;
                 } else if (this.date.length == 7) {
+                    return true;
+                }
+                return false;
+            },
+            isNumberValid: function() {
+                if (this.number == '' || this.number == undefined) {
+                    return null;
+                } else if (this.number.length <= 7) {
                     return true;
                 }
                 return false;
@@ -55,7 +67,7 @@ if (Vue) {
         methods: {
             format: function(val, e) {
                 // to handle input field content
-                return val;
+                return val.replace(/\D/g, '').substr(0, 7);
             },
             popup: function(e) {
                 showModal({
