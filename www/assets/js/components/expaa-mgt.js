29b728aaa5ab6c4885e10c1d4b9110cb0fba87ff
pyliu <3634168+pyliu@users.noreply.github.com>
2020-01-02 21:03:38 +0800

    add obsolete modal impl

--- www/assets/js/components/expaa-mgt.js
+++ www/assets/js/components/expaa-mgt.js
@@ -39,7 +39,7 @@ if (Vue) {
                     &ensp;
                     <button @click="popup" class="btn btn-sm btn-outline-success">備註</button>
                     &ensp;
-                    <button id="expaa_add_obsolete_button" class="btn btn-sm btn-outline-danger" title="新增作廢假資料以利空白規費單作廢"><i class="fas fa-ban"></i></button>
+                    <button @click="obsolete" class="btn btn-sm btn-outline-danger" title="新增作廢假資料"><i class="fas fa-ban"></i></button>
                 </div>
             </b-container>
         </fieldset>`,
@@ -119,6 +119,56 @@ if (Vue) {
                     `,
                     size: "lg"
                 });
+            },
+            obsolete: function(e) {
+                // query first then do the creation
+                let body = new FormData();
+                body.append("type", "get_dummy_ob_fees");
+
+                toggle(e.target);
+
+                fetch("query_json_api.php", {
+                    method: "POST",
+                    body: body
+                }).then(response => {
+                    if (response.status != 200) {
+                        throw new Error("XHR連線異常，回應非200");
+                    }
+                    return response.json();
+                }).then(jsonObj => {
+                    toggle(e.target);
+                    var now = new Date();
+                    let last_pc_number = jsonObj.raw ? jsonObj.raw[0]["AA04"] : 0;
+                    let today = (now.getFullYear() - 1911) +
+                        ("0" + (now.getMonth() + 1)).slice(-2) +
+                        ("0" + now.getDate()).slice(-2);
+                    let h =  this.$createElement;
+                    let VNode = h("expaa-obsolete-mgt", {
+                        props: {
+                            raw_data: jsonObj.raw
+                        }
+                    });
+                    
+                    showModal({
+                        title: "規費作廢假資料",
+                        message: VNode,
+                        size: "lg",
+                        callback: () => {
+                            $("#add_dummy_expaa_btn").off("click").on("click", xhrAddDummyObsoleteFeesData.bind({
+                                pc_number: last_pc_number,
+                                today: today
+                            }));
+                            addUserInfoEvent();
+                        }
+                    });
+                }).catch(ex => {
+                    console.error("xhrQueryObsoleteFees parsing failed", ex);
+                    showAlert({
+                        title: "查詢作廢規費回應不正常",
+                        message: ex.message,
+                        type: "danger"
+                    });
+                });
             }
         },
         created: function() {
@@ -140,6 +190,83 @@ if (Vue) {
                         toValue: (date, format, language) => new Date()
                     }
                 });
+            }
+        },
+        components: {
+            "expaa-obsolete-mgt": {
+                template: `<div class="small">
+                    下一筆假資料：<br />
+                    ※ 電腦給號：{{next_pc_number}} <br />
+                    ※ 日期：{{today}} <br />
+                    <div class="form-row">
+                        <div class="input-group input-group-sm col-3">
+                            <div class="input-group-prepend">
+                                <span class="input-group-text bg-danger text-white" id="inputGroup-operator">作業人員</span>
+                            </div>
+                            <input v-model="operator" id="dummy_operator" type="text" placeholder="HB1128" class="form-control" aria-label="作業人員" aria-describedby="inputGroup-operator" required>
+                        </div>
+                        <div class="input-group input-group-sm col-3">
+                            <div class="input-group-prepend">
+                                <span class="input-group-text bg-danger text-white" id="inputGroup-fee-number">收據號碼</span>
+                            </div>
+                            <input v-model="AB_number" id="dummy_fee_number" type="text" placeholder="AB00099480" class="form-control" aria-label="收據號碼" aria-describedby="inputGroup-fee-number" required>
+                        </div>
+                        <div class="input-group input-group-sm col">
+                            <div class="input-group-prepend">
+                                <span class="input-group-text bg-danger text-white" id="inputGroup-obsolete-reason">作廢原因</span>
+                            </div>
+                            <input v-model="reason" id="dummy_obsolete_reason" type="text" placeholder="空白單據作廢" class="form-control" aria-label="作廢原因" aria-describedby="inputGroup-obsolete-reason" required>
+                        </div>
+                        <div class="btn-group-sm col-1" role="group">
+                            <button class="btn btn-outline-primary">新增</button>
+                        </div>
+                    </div>
+                    <hr>目前系統中({{year}}年度)的假資料有 {{count}} 筆：<br />
+                    <table class="table text-center">
+                        <tr>
+                            <th>日期</th>
+                            <th>電腦給號</th>
+                            <th>收據編號</th>
+                            <th>作廢原因</th>
+                            <th>作業人員</th>
+                        </tr>
+                        <tr v-for="item in raw_data">
+                            <td>{{item["AA01"]}}</td>
+                            <td>{{item["AA04"]}}</td>
+                            <td>{{item["AA05"]}}</td>
+                            <td>{{item["AA104"]}}</td>
+                            <td><span data-id="{{item["AA39"]}}" class="user_tag">{{item["AA39"]}}</span></td>
+                        </tr>
+                    </table>
+                </div>`,
+                props: ["raw_data"],
+                data: function() {
+                    return {
+                        year: "109",
+                        next_pc_number: "9109001",  // 9 + year (3 digits) + serial (3 digits)
+                        today: "",
+                        operator: "",   // 作業人員
+                        AB_number: "",  // 收據編號
+                        reason: ""      // 作廢原因
+                    }
+                },
+                computed: {
+                    count: function() {
+                        return this.raw_data.length;
+                    }
+                },
+                methods: {
+                    
+                },
+                created: function() {
+                    var now = new Date();
+                    this.year = now.getFullYear() - 1911;
+                    this.today = this.year +
+                        ("0" + (now.getMonth() + 1)).slice(-2) +
+                        ("0" + now.getDate()).slice(-2);
+                    if (!this.raw_data) this.raw_data = [];
+                    this.next_pc_number = this.raw_data.length > 0 ? this.raw_data[0]["AA04"] : `9${this.year}001`;
+                }
             }
         }
     });
