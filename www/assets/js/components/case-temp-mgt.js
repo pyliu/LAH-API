e663134804fef0b763262c08d6a8a5a05cf1b132
pyliu <3634168+pyliu@users.noreply.github.com>
2020-01-07 20:01:42 +0800

    modify case-temp-mgt ui

--- www/assets/js/components/case-temp-mgt.js
+++ www/assets/js/components/case-temp-mgt.js
@@ -2,15 +2,20 @@ if (Vue) {
     Vue.component("case-temp-mgt", {
         template: `<fieldset>
             <legend>暫存檔查詢</legend>
-            <div class="form-row">
-            <div class="col-8">
-                <case-input-group-ui @update="handleUpdate" @enter="query" type="reg" prefix="case_temp"></case-input-group-ui>
-            </div>
-            <div class="filter-btn-group col-4">
-                <b-button @click="query" variant="outline-primary" size="sm"><i class="fas fa-search"></i> 查詢</b-button>
-                <b-button @click="popup" variant="outline-success" size="sm"><i class="far fa-comment"></i> 備註</b-button>
-            </div>
-            </div>
+            <b-row class="mb-2">
+                <b-col>
+                    <case-input-group-ui @update="handleUpdate" @enter="query" type="reg" prefix="case_temp"></case-input-group-ui>
+                </b-col>
+            </b-row>
+            <b-row no-gutters>
+                <b-col>
+                    <b-button block pill @click="query" variant="outline-primary" size="sm"><i class="fas fa-search"></i> 查詢</b-button>
+                </b-col>
+                &ensp;
+                <b-col>
+                    <b-button block pill @click="popup" variant="outline-success" size="sm"><i class="far fa-comment"></i> 備註</b-button>
+                </b-col>
+            </b-row>
         </fieldset>`,
         data: () => {
             return {
