ec1091bc615c3695e92db3e51c5df42bc3116351
pyliu <3634168+pyliu@users.noreply.github.com>
2020-01-07 19:56:49 +0800

    modify case-state-mgt ui

--- www/assets/js/components/case-state-mgt.js
+++ www/assets/js/components/case-state-mgt.js
@@ -1,16 +1,21 @@
 if (Vue) {
     Vue.component("case-state-mgt", {
         template: `<fieldset>
-            <legend>案件狀態調整</legend>
-            <div class="form-row">
-            <div class="col-8">
-                <case-input-group-ui @update="handleUpdate" @enter="query" type="reg" prefix="case_state"></case-input-group-ui>
-            </div>
-            <div class="filter-btn-group col-4">
-                <b-button @click="query" variant="outline-primary" size="sm"><i class="fas fa-search"></i> 查詢</b-button>
-                <b-button @click="popup" variant="outline-success" size="sm"><i class="far fa-comment"></i> 備註</b-button>
-            </div>
-            </div>
+            <legend>案件狀態</legend>
+            <b-row class="mb-2">
+                <b-col>
+                    <case-input-group-ui @update="handleUpdate" @enter="query" type="reg" prefix="case_state"></case-input-group-ui>
+                </b-col>
+            </b-row>
+            <b-row no-gutters>
+                <b-col>
+                    <b-button block pill @click="query" variant="outline-primary" size="sm"><i class="fas fa-search"></i> 查詢</b-button>
+                </b-col>
+                &ensp;
+                <b-col>
+                    <b-button block pill  @click="popup" variant="outline-success" size="sm"><i class="far fa-comment"></i> 備註</b-button>
+                </b-col>
+            </b-row>
         </fieldset>`,
         data: () => {
             return {
