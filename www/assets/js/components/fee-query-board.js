5d3346895b6d83a1ad1d8d8ee73d7638d0a004dc
pyliu <3634168+pyliu@users.noreply.github.com>
2020-01-14 13:38:09 +0800

    layout arrange

--- www/assets/js/components/fee-query-board.js
+++ www/assets/js/components/fee-query-board.js
@@ -36,10 +36,11 @@ if (Vue) {
                     </b-input-group>
                 </b-col>
             </b-form-row>
-            <b-form-row>
-                <b-col><b-button pill block @click="query" variant="outline-primary" size="sm"><i class="fas fa-search"></i> 查詢</b-button></b-col>
+            <b-form-row class="text-center">
                 <b-col>
-                    <b-button block pill @click="obsolete" variant="outline-secondary" size="sm" v-b-tooltip.bottom="'新增作廢假資料'">
+                    <b-button pill block @click="query" variant="outline-primary" size="sm"><i class="fas fa-search"></i> 查詢</b-button>
+                    <b-button pill block @click="popup" variant="outline-success" size="sm"><i class="far fa-comment"></i> 備註</b-button>
+                    <b-button pill block @click="obsolete" variant="outline-secondary" size="sm" v-b-tooltip.bottom="'新增作廢假資料'">
                         <span class="fa-stack" style="font-size: 0.5rem">
                             <i class="fas fa-file-alt fa-stack-1x"></i>
                             <i class="fas fa-ban fa-stack-2x text-danger"></i>
@@ -47,7 +48,6 @@ if (Vue) {
                         作廢
                     </b-button>
                 </b-col>
-                <b-col><b-button block pill @click="popup" variant="outline-success" size="sm"><i class="far fa-comment"></i> 備註</b-button></b-col>
             </b-form-row>
         </fieldset>`,
         data: () => {
