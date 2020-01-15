243d81bca66916b3c13fa1df39da45b3e09c41b4
pyliu <3634168+pyliu@users.noreply.github.com>
2020-01-15 13:20:54 +0800

    add case-sur-dialog component to handle fix function

--- www/assets/js/components/case-sur-mgt.js
+++ www/assets/js/components/case-sur-mgt.js
@@ -79,38 +79,19 @@ if (Vue) {
                     });
                 });
             },
-            dialog: (jsonObj) => {
+            dialog: function(jsonObj) {
                 if (jsonObj.status == XHR_STATUS_CODE.DEFAULT_FAIL) {
-                    let html = "收件字號：" + "<a title='案件辦理情形 on " + landhb_svr + "' href='#' onclick='javascript:window.open(\"http://\"\+landhb_svr\+\":9080/LandHB/Dispatcher?REQ=CMC0202&GRP=CAS&MM01="+ jsonObj.raw["MM01"] +"&MM02="+ jsonObj.raw["MM02"] +"&MM03="+ jsonObj.raw["MM03"] +"&RM90=\")'>" + jsonObj.收件字號 + "</a> </br>";
-                    html += "收件時間：" + jsonObj.收件時間 + " <br/>";
-                    html += "收件人員：" + jsonObj.收件人員 + " <br/>";
-                    html += "　連件數：<input type='text' id='mm24_upd_text' value='" + jsonObj.raw["MM24"] + "' /> <button id='mm24_upd_btn' data-table='SCMSMS' data-case-id='" + jsonObj.收件字號.replace(/[^a-zA-Z0-9]/g, "") + "' data-origin-value='" + jsonObj.raw["MM24"] + "' data-column='MM24' data-input-id='mm24_upd_text' data-title=' " + jsonObj.raw["MM01"] + "-" + jsonObj.raw["MM02"] + "-" + jsonObj.raw["MM03"] + " 連件數'>更新</button><br/>";
-                    html += "申請事由：" + jsonObj.raw["MM06"] + "：" + jsonObj.申請事由 + " <br/>";
-                    html += "　段小段：" + jsonObj.raw["MM08"] + " <br/>";
-                    html += "　　地號：" + (isEmpty(jsonObj.raw["MM09"]) ? "" : jsonObj.地號) + " <br/>";
-                    html += "　　建號：" + (isEmpty(jsonObj.raw["MM10"]) ? "" : jsonObj.建號) + " <br/>";
-                    html += "<span class='text-info'>辦理情形</span>：" + jsonObj.辦理情形 + " <br/>";
-                    html += "結案狀態：" + jsonObj.結案狀態 + " <br/>";
-                    html += "<span class='text-info'>延期原因</span>：" + jsonObj.延期原因 + " <br/>";
-                    html += "<span class='text-info'>延期時間</span>：" + jsonObj.延期時間 + " <br/>";
-                    if (jsonObj.結案已否 && jsonObj.raw["MM22"] == "C") {
-                        html += '<h6 class="mt-2 mb-2"><span class="text-danger">※</span> ' + "發現 " + jsonObj.收件字號 + " 已「結案」但辦理情形為「延期複丈」!" + '</h6>';
-                        html += "<button id='sur_delay_case_fix_button' class='text-danger' data-trigger='manual' data-toggle='popover' data-content='需勾選右邊其中一個選項才能進行修正' title='錯誤訊息' data-placement='top'>修正</button> ";
-                        html += "<label for='sur_delay_case_fix_set_D'><input id='sur_delay_case_fix_set_D' type='checkbox' checked /> 辦理情形改為核定</label> ";
-                        html += "<label for='sur_delay_case_fix_clear_delay_datetime'><input id='sur_delay_case_fix_clear_delay_datetime' type='checkbox' checked /> 清除延期時間</label> ";
-                    }
                     showModal({
                         title: "測量案件查詢",
-                        body: html,
-                        size: "md",
+                        message: this.$createElement("case-sur-dialog", { props: { json: jsonObj } }),
                         callback: function() {
-                            $("#sur_delay_case_fix_button").off("click").one("click", xhrFixSurDelayCase.bind(jsonObj.收件字號));
-                            $("#mm24_upd_btn").off("click").one("click", e => {
-                                // input validation
-                                let number = $("#mm24_upd_text").val().replace(/\D/g, "");
-                                $("#mm24_upd_text").val(number);
-                                xhrUpdateCaseColumnData(e);
-                            });
+                            // $("#sur_delay_case_fix_button").off("click").one("click", xhrFixSurDelayCase.bind(jsonObj.收件字號));
+                            // $("#mm24_upd_btn").off("click").one("click", e => {
+                            //     // input validation
+                            //     let number = $("#mm24_upd_text").val().replace(/\D/g, "");
+                            //     $("#mm24_upd_text").val(number);
+                            //     xhrUpdateCaseColumnData(e);
+                            // });
                             addUserInfoEvent();
                         }
                     });
@@ -127,6 +108,142 @@ if (Vue) {
                     <div>原因是 CMB0301 延期複丈功能，針對於有連件案件在做處理時，會自動根據MM24案件數，將後面的案件自動做延期複丈的更新。導致後續已結案的案件會被改成延期複丈的狀態 MM22='C' 就是 100、200、300、400為四連件，所以100的案件 MM24='4'，200、300、400 的 MM24='0' 延期複丈的問題再將100號做延期複丈的時候，會將200、300、400也做延期複丈的更新，所以如果400已經結案，100做延期複丈，那400號就會變成 MM22='C' MM23='A' MM24='4' 的異常狀態。</div>`,
                     size: "lg"
                 });
+            }
+        },
+        components: {
+            "case-sur-dialog": {
+                template: `<div>
+                    收件字號：<a title="案件辦理情形 on ${landhb_svr}" :href="'http://${landhb_svr}:9080/LandHB/Dispatcher?REQ=CMC0202&GRP=CAS&MM01=' + json.raw['MM01'] + '&MM02=' + json.raw['MM02'] + '&MM03=' + json.raw['MM03'] + '&RM90='" target="_blank">{{json.收件字號}}</a> </br>
+                    收件時間：{{json.收件時間}} <br/>
+                    收件人員：<span v-html="json.收件人員"></span> <br/>
+                    <b-form-row class="w-100">
+                        <b-col cols="4">
+                            <b-input-group size="sm">
+                                <b-input-group-prepend is-text>連件數</b-input-group-prepend>
+                                <b-form-input
+                                    v-model="count"
+                                    id='mm24_upd_text'
+                                    type="number"
+                                    min="0"
+                                    inline
+                                ></b-form-input>
+                            </b-input-group>
+                        </b-col>
+                        <b-col>
+                            <b-button
+                                id='mm24_upd_btn'
+                                data-table='SCMSMS'
+                                :data-case-id="json.收件字號.replace(/[^a-zA-Z0-9]/g, '')"
+                                :data-origin-value="json.raw['MM24']"
+                                data-column='MM24'
+                                data-input-id='mm24_upd_text'
+                                :data-title="json.raw['MM01'] + '-' + json.raw['MM02'] + '-' + json.raw['MM03'] + '連件數'"
+                                size="sm"
+                                variant="outline-primary"
+                            >更新</b-button>
+                        </b-col>
+                    </b-form-row>
+                    申請事由：{{json.raw["MM06"]}}：{{json.申請事由}} <br/>
+                    　段小段：{{json.raw["MM08"]}} <br/>
+                    　　地號：{{isEmpty(json.raw["MM09"]) ? "" : this.json.地號}} <br/>
+                    　　建號：{{isEmpty(json.raw["MM10"]) ? "" : this.json.建號}} <br/>
+                    <span class='text-info'>辦理情形</span>：{{json.辦理情形}} <br/>
+                    結案狀態：{{json.結案狀態}} <br/>
+                    <span class='text-info'>延期原因</span>：{{json.延期原因}} <br/>
+                    <span class='text-info'>延期時間</span>：{{json.延期時間}} <br/>
+                    <div v-if="json.結案已否 && json.raw['MM22'] == 'C'">
+                        <h6 class="mt-2 mb-2"><span class="text-danger">※</span> 發現 {{json.收件字號}} 已「結案」但辦理情形為「延期複丈」!</h6>
+                        <b-button
+                            variant="outline-danger"
+                            id="sur_delay_case_fix_button"
+                            data-trigger="manual"
+                            data-toggle="popover"
+                            data-content="需勾選右邊其中一個選項才能進行修正"
+                            title="錯誤訊息"
+                            data-placement="top"
+                            size="sm"
+                            @click="fix"
+                        >修正</b-button>
+                        <b-form-checkbox
+                            id='sur_delay_case_fix_set_D'
+                            v-model="setD"
+                            size="sm"
+                            inline
+                        >辦理情形改為核定</b-form-checkbox>
+                        <b-form-checkbox
+                            id='sur_delay_case_fix_clear_delay_datetime'
+                            type='checkbox'
+                            v-model="clearDatetime"
+                            size="sm"
+                            inline
+                        >清除延期時間</b-form-checkbox>
+                        <b-popover
+                            :disabled.sync="disabled_popover"
+                            target="sur_delay_case_fix_button"
+                            title="錯誤提示"
+                            ref="popover"
+                            placement="top"
+                        >
+                            需勾選右邊其中一個選項才能進行修正
+                        </b-popover>
+                    </div>
+                    <div v-if="debug">{{setD}}, {{clearDatetime}}, {{count}}</div>
+                </div>`,
+                props: ["json"],
+                data: () => {
+                    return {
+                        setD: true,
+                        clearDatetime: true,
+                        count: 0,
+                        disabled_popover: true,
+                        debug: false
+                    }
+                },
+                methods: {
+                    fix: function(e) {
+                        if (!this.setD && !this.clearDatetime) {
+                            this.disabled_popover = false;
+                            return;
+                        }
+                        this.disabled_popover = true;
+                        let id = `${this.json.raw['MM01']}${this.json.raw['MM02']}${this.json.raw['MM03']}`;
+                        let upd_mm22 = this.setD;
+                        let clr_delay = this.clearDatetime;
+                        showConfirm("確定要修正本案件?", function() {
+                            $(e.target).remove();
+                            //fix_sur_delay_case
+                            let body = new FormData();
+                            body.append("type", "fix_sur_delay_case");
+                            body.append("id", id);
+                            body.append("UPD_MM22", upd_mm22);
+                            body.append("CLR_DELAY", clr_delay);
+                            asyncFetch("query_json_api.php", {
+                                method: "POST",
+                                body: body
+                            }).then(jsonObj => {
+                                if (jsonObj.status == XHR_STATUS_CODE.SUCCESS_NORMAL) {
+                                    addNotification({
+                                        type: "success",
+                                        message: id + " 複丈案件修正成功!"
+                                    });
+                                } else {
+                                    let msg = "回傳狀態碼不正確!【" + jsonObj.message + "】";
+                                    showAlert({
+                                        title: "修正複丈案件失敗",
+                                        message: msg,
+                                        type: "danger"
+                                    });
+                                }
+                            }).catch(ex => {
+                                console.error("case-sur-dialog::fix parsing failed", ex);
+                                showAlert({title: "修正複丈案件失敗", message: "修正 " + id + " 失敗!【" + ex.toString() + "】", type: "danger"});
+                            });
+                        });
+                    }
+                },
+                mounted: function() {
+                    this.count = this.json.raw["MM24"];
+                }
             }
         }
     });
