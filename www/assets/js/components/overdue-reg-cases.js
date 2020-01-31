3deccbcfbbb536378760ac6c4aeec46b25947cdc
pyliu <3634168+pyliu@users.noreply.github.com>
2020-01-31 13:51:04 +0800

    add load function in overdue-reg-cases component

--- www/assets/js/components/overdue-reg-cases.js
+++ www/assets/js/components/overdue-reg-cases.js
@@ -43,28 +43,37 @@ if (Vue) {
                 ],
                 height: true,
                 caption: "查詢中 ... ",
-                busy: true
+                busy: true,
+                timer_handle: null
+            }
+        },
+        methods: {
+            load: function() {
+                this.busy = true;
+                let form_body = new FormData();
+                form_body.append("type", "overdue_reg_cases");
+                asyncFetch("query_json_api.php", {
+                    method: 'POST',
+                    body: form_body
+                }).then(jsonObj => {
+                    console.assert(jsonObj.status == XHR_STATUS_CODE.SUCCESS_NORMAL, "查詢登記逾期案件回傳狀態碼有問題【" + jsonObj.status + "】");
+                    this.busy = false;
+                    this.items = jsonObj.items;
+                    this.caption = `${jsonObj.data_count} 件，更新時間: ${new Date()}`;
+                    setTimeout(() => {
+                        $("table tr td:nth-child(2)").on("click", window.utilApp.fetchRegCase).addClass("reg_case_id");
+                        addNotification({ title: "查詢登記逾期案件", message: `查詢到 ${jsonObj.data_count} 件案件`, type: "success" });
+                    }, 1000);
+                }).catch(ex => {
+                    console.error("overdue-reg-cases::created parsing failed", ex);
+                    showAlert({message: "overdue-reg-cases::created XHR連線查詢有問題!!【" + ex + "】", type: "danger"});
+                });
             }
         },
         created() {
-            let form_body = new FormData();
-            form_body.append("type", "overdue_reg_cases");
-            asyncFetch("query_json_api.php", {
-                method: 'POST',
-                body: form_body
-            }).then(jsonObj => {
-                console.assert(jsonObj.status == XHR_STATUS_CODE.SUCCESS_NORMAL, "查詢登記逾期案件回傳狀態碼有問題【" + jsonObj.status + "】");
-                this.busy = false;
-                this.items = jsonObj.items;
-                this.caption = `查詢到 ${jsonObj.data_count} 件`;
-                setTimeout(() => {
-                    $("table tr td:nth-child(2)").on("click", window.utilApp.fetchRegCase).addClass("reg_case_id");
-                    addNotification({ title: "查詢登記逾期案件", message: `查詢到 ${jsonObj.data_count} 件案件`, type: "success" });
-                }, 1000);
-            }).catch(ex => {
-                console.error("overdue-reg-cases::created parsing failed", ex);
-                showAlert({message: "overdue-reg-cases::created XHR連線查詢有問題!!【" + ex + "】", type: "danger"});
-            });
+            this.load();
+            // reload the table every 15 mins
+            this.timer_handle = setInterval(this.load, 15 * 60 * 1000);
         },
         mounted() {
             this.height = $(document).height() - 145 + "px";
