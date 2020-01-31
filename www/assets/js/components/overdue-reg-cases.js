2d5c7e112add494534e0e3179585ea1463a5144c
pyliu <3634168+pyliu@users.noreply.github.com>
2020-01-31 11:35:49 +0800

    update b-table usage

--- www/assets/js/components/overdue-reg-cases.js
+++ www/assets/js/components/overdue-reg-cases.js
@@ -6,15 +6,44 @@ if (Vue) {
                 hover
                 responsive
                 bordered
-                head-variant="light"
+                head-variant="dark"
+                caption-top
+                :caption="caption"
                 :sticky-header="height"
                 :items="items"
-            ></b-table>
+                :fields="fields"
+                :busy="busy"
+            >
+                <template v-slot:table-busy>
+                    <div class="text-center text-danger my-5">
+                        <b-spinner class="align-middle"></b-spinner>
+                        <strong>查詢中 ...</strong>
+                    </div>
+                </template>
+                <template v-slot:cell(序號)="data">
+                    {{data.index + 1}}
+                </template>
+                <template v-slot:cell(初審人員)="data">
+                    <b class="text-info">{{data.value}}</b>
+                </template>
+            </b-table>
         </div>`,
         data: function () {
             return {
                 items: [],
-                height: true
+                fields: [
+                    '序號',
+                    {key: "收件字號", sortable: true},
+                    {key: "登記原因", sortable: true},
+                    {key: "辦理情形", sortable: true},
+                    {key: "初審人員", sortable: true},
+                    {key: "作業人員", sortable: true},
+                    {key: "限辦期限", sortable: true},
+                    {key: "收件時間", sortable: true}
+                ],
+                height: true,
+                caption: "查詢中 ... ",
+                busy: true
             }
         },
         created() {
@@ -25,8 +54,13 @@ if (Vue) {
                 body: form_body
             }).then(jsonObj => {
                 console.assert(jsonObj.status == XHR_STATUS_CODE.SUCCESS_NORMAL, "查詢登記逾期案件回傳狀態碼有問題【" + jsonObj.status + "】");
+                this.busy = false;
                 this.items = jsonObj.items;
-                addNotification({ title: "查詢登記逾期案件", message: `查詢到${jsonObj.data_count}件案件`, type: "success" });
+                this.caption = `查詢到 ${jsonObj.data_count} 件`;
+                setTimeout(() => {
+                    $("table tr td:nth-child(2)").on("click", window.utilApp.fetchRegCase).addClass("reg_case_id");
+                    addNotification({ title: "查詢登記逾期案件", message: `查詢到 ${jsonObj.data_count} 件案件`, type: "success" });
+                }, 1000);
             }).catch(ex => {
                 console.error("overdue-reg-cases::created parsing failed", ex);
                 showAlert({message: "overdue-reg-cases::created XHR連線查詢有問題!!【" + ex + "】", type: "danger"});
@@ -34,9 +68,6 @@ if (Vue) {
         },
         mounted() {
             this.height = $(document).height() - 145 + "px";
-            setTimeout(() => {
-                $("table tr td:first-child").on("click", window.utilApp.fetchRegCase).addClass("reg_case_id");
-            }, 1000);
         }
     });
 } else {
