fd9a29efb3750a5c563406fe05b644da23a3ba66
pyliu <3634168+pyliu@users.noreply.github.com>
2020-02-01 21:49:03 +0800

    wip

--- www/assets/js/components/watchdog.js
+++ www/assets/js/components/watchdog.js
@@ -1,23 +1,40 @@
 if (Vue) {
+    Vue.component(VueCountdown.name, VueCountdown);
     Vue.component("watchdog", {
         template: `<b-row>
             <b-col>
-                <b-card  title="排程" sub-title="顯示最近排程執行歷程">
-                    <b-card-text v-for="item in schedule_history">{{item}}</b-card-text>
+                <b-card header="排程儀表版">
+                    <countdown ref="countdown" :time="timer_milliseconds" :auto-start="false">
+                        <template slot-scope="props">下次執行 {{ props.minutes }}:{{ props.seconds }}</template>
+                    </countdown>
+                    <b-list-group flush>
+                        <b-list-group-item v-for="item in schedule_history">{{item}}</b-list-group-item>
+                    </b-list-group>
                 </b-card>
             </b-col>
             <b-col>
-                <b-card  title="記錄檔" sub-title="顯示最近記錄檔"></b-card>
+                <b-card header="紀錄儀表版"></b-card>
             </b-col>
         </b-row>`,
         data: function () {
             return {
                 schedule_history: [],
                 watchdog_timer: null,
+                timer_milliseconds: 15 * 60 * 1000,  // 15 minutes
                 log_list: []
             }
         },
         methods: {
+            resetCountdown: function () {
+                this.$refs.countdown.totalMilliseconds = this.timer_milliseconds;
+                this.$refs.countdown.start();
+            },
+            startCountdown: function () {
+                this.$refs.countdown.start();
+            },
+            endCountdown: function () {
+                this.$refs.countdown.totalMilliseconds = 0;
+            },
             callWatchdogAPI: function() {
                 // generate current date time string
                 let dt = new Date();
@@ -29,18 +46,20 @@ if (Vue) {
                     method: "POST",
                     body: body
                 }).then(jsonObj => {
-                    this.schedule_history.push(`${now} result: ${jsonObj.status}`);
                     // normal success jsonObj.status == XHR_STATUS_CODE.SUCCESS_NORMAL
-                    if (jsonObj.status == XHR_STATUS_CODE.SUCCESS_NORMAL) {       
-                        // Backend will check if it needs to go or not
-                        this.watchdog_timer = setTimeout(this.callWatchdogAPI, 1000 * 60 * 15);	// call the watchdog every 15 mins         
+                    if (jsonObj.status == XHR_STATUS_CODE.SUCCESS_NORMAL) {
+                        this.schedule_history.push(`${now} 結果: OK`);
+                        // Backend will check if it needs to do or not
+                        this.watchdog_timer = setTimeout(this.callWatchdogAPI, this.timer_milliseconds);	// call the watchdog every 15 mins
+                        this.startCountdown();
                     } else {
                         // stop the timer if API tells it is not working
-                        this.schedule_history.push(`${now} result: ${jsonObj.message}`);
+                        this.schedule_history.push(`${now} 結果: ${jsonObj.message}`);
                         console.warn(jsonObj.message);
                     }
                 }).catch(ex => {
-                    this.schedule_history.push(`${now} result: ${ex.message}`);
+                    this.endCountdown();
+                    this.schedule_history.push(`${now} 結果: ${ex.message}`);
                     showAlert({
                         title: 'watchdog::callWatchdogAPI parsing failed',
                         message: ex.message,
@@ -48,10 +67,18 @@ if (Vue) {
                     });
                     console.error("watchdog::callWatchdogAPI parsing failed", ex);
                 });
+            },
+            getTodayLog: function() {
+                let dt = new Date();
+                let log_filename = `${dt.getFullYear()}-${(dt.getMonth()+1).toString().padStart(2, '0')}-${(dt.getDate().toString().padStart(2, '0'))}.log`
+                jQuery.get(`logs/${log_filename}`, function(data) {
+                    alert(data);
+                });
             }
         },
         mounted() {
             this.callWatchdogAPI();
+            this.getTodayLog();
         }
     });
 } else {
