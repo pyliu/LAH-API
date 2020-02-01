6db3ebea74dc6ed758e1388b251a53be79e1f37c
pyliu <3634168+pyliu@users.noreply.github.com>
2020-02-02 01:35:58 +0800

    add load log function

--- www/assets/js/components/watchdog.js
+++ www/assets/js/components/watchdog.js
@@ -4,16 +4,42 @@ if (Vue) {
         template: `<b-row>
             <b-col>
                 <b-card header="排程儀表版">
-                    <countdown ref="countdown" :time="timer_milliseconds" :auto-start="false">
-                        <template slot-scope="props">下次執行 {{ props.minutes }}:{{ props.seconds }}</template>
-                    </countdown>
-                    <b-list-group flush>
-                        <b-list-group-item v-for="item in schedule_history">{{item}}</b-list-group-item>
-                    </b-list-group>
+                    <b-row>
+                        <b-col cols="8">
+                            <countdown ref="countdown" :time="timer_milliseconds" :auto-start="false">
+                                <template slot-scope="props">{{ props.minutes }}:{{ props.seconds }} 後自動執行</template>
+                            </countdown>
+                        </b-col>
+                        <b-col cols="4">
+                            <b-input-group size="sm">
+                                <b-input-group-prepend is-text>顯示個數</b-input-group-prepend>
+                                <b-form-input
+                                    type="number"
+                                    v-model="display_count"
+                                    size="sm"
+                                ></b-form-input>
+                            </b-input-group>
+                        </b-col>
+                    </b-row>
+                    <small>
+                        <b-list-group flush>
+                            <b-list-group-item v-for="item in schedule_history">{{item}}</b-list-group-item>
+                        </b-list-group>
+                    </small>
                 </b-card>
             </b-col>
             <b-col>
-                <b-card header="紀錄儀表版"></b-card>
+                <b-card bo-body header="紀錄儀表版">
+                    <div class="d-flex w-100 justify-content-between">
+                        <b-button variant="outline-primary" size="sm" @click="callLogAPI">刷新</b-button>
+                        <small class="text-muted">更新時間: {{log_update_time}}</small>
+                    </div>
+                    <small>
+                        <b-list-group flush>
+                            <b-list-group-item v-for="item in log_list">{{item}}</b-list-group-item>
+                        </b-list-group>
+                    </small>
+                </b-card>
             </b-col>
         </b-row>`,
         data: function () {
@@ -21,7 +47,9 @@ if (Vue) {
                 schedule_history: [],
                 watchdog_timer: null,
                 timer_milliseconds: 15 * 60 * 1000,  // 15 minutes
-                log_list: []
+                log_list: [],
+                log_update_time: "08:10:11",
+                display_count: 10
             }
         },
         methods: {
@@ -36,10 +64,52 @@ if (Vue) {
                 this.$refs.countdown.totalMilliseconds = 0;
             },
             addScheduleHistory: function (message) {
-                if (this.schedule_history.length > 4) {
+                if (this.schedule_history.length == this.display_count) {
                     this.schedule_history.pop();
+                } else if (this.schedule_history.length > this.display_count) {
+                    this.schedule_history = [];
                 }
                 this.schedule_history.unshift(message);
+            },
+            addLogList: function (message) {
+                if (this.log_list.length == this.display_count) {
+                    this.log_list.pop();
+                } else if (this.log_list.length > this.display_count) {
+                    this.log_list = [];
+                }
+                this.log_list.unshift(message);
+            },
+            callLogAPI: function () {
+                let dt = new Date();
+                this.log_update_time = `${dt.getHours().toString().padStart(2, '0')}:${dt.getMinutes().toString().padStart(2, '0')}:${dt.getSeconds().toString().padStart(2, '0')}`;
+                let log_filename = `${dt.getFullYear()}-${(dt.getMonth()+1).toString().padStart(2, '0')}-${(dt.getDate().toString().padStart(2, '0'))}.log`
+                let body = new FormData();
+                body.append("type", "load_log");
+                body.append("log_filename", log_filename);
+                asyncFetch("load_file_api.php", {
+                    method: "POST",
+                    body: body
+                }).then(jsonObj => {
+                    // normal success jsonObj.status == XHR_STATUS_CODE.SUCCESS_NORMAL
+                    if (jsonObj.status == XHR_STATUS_CODE.SUCCESS_NORMAL) {
+                        let that = this;
+                        jsonObj.data.forEach(function(item, index, array){
+                            that.addLogList(item);
+                        });
+                    } else {
+                        // stop the timer if API tells it is not working
+                        this.addLogList(`${this.log_update_time} 錯誤: ${jsonObj.message}`);
+                        console.warn(jsonObj.message);
+                    }
+                }).catch(ex => {
+                    this.addLogList(`${this.log_update_time} 錯誤: ${ex.message}`);
+                    showAlert({
+                        title: 'watchdog::callLogAPI parsing failed',
+                        message: ex.message,
+                        type: 'danger'
+                    });
+                    console.error("watchdog::callLogAPI parsing failed", ex);
+                });
             },
             callWatchdogAPI: function() {
                 // generate current date time string
@@ -73,18 +143,11 @@ if (Vue) {
                     });
                     console.error("watchdog::callWatchdogAPI parsing failed", ex);
                 });
-            },
-            getTodayLog: function() {
-                let dt = new Date();
-                let log_filename = `${dt.getFullYear()}-${(dt.getMonth()+1).toString().padStart(2, '0')}-${(dt.getDate().toString().padStart(2, '0'))}.log`
-                jQuery.get(`logs/${log_filename}`, function(data) {
-                    alert(data);
-                });
             }
         },
         mounted() {
             this.callWatchdogAPI();
-            this.getTodayLog();
+            this.callLogAPI();
         }
     });
 } else {
--- www/load_file_api.php
+++ www/load_file_api.php
@@ -26,6 +26,22 @@ switch ($_POST["type"]) {
             echoErrorJSONString("找不到檔案【".$path."】");
         }
         break;
+    case "load_log":
+        $log->info("XHR [load_log] 查詢請求【".$_POST["log_filename"]."】");
+        $path = "./logs/".$_POST["log_filename"];
+        if (file_exists($path)) {
+            $result = array(
+                "status" => STATUS_CODE::SUCCESS_NORMAL,
+                "data" => explode("\n", file_get_contents($path)),  // line by line
+                "query_string" => "log_filename=".$_POST["log_filename"]."&type=".$_POST["type"]
+            );
+            $log->info("XHR [load_log] 讀取成功【".$path."】");
+            echo json_encode($result, 0);
+        } else {
+            $log->error("XHR [load_log] 找不到檔案【".$path."】");
+            echoErrorJSONString("找不到檔案【".$path."】");
+        }
+        break;
     default:
         $log->error("XHR [".$_POST["type"]."] 不支援的讀取型態");
         echoErrorJSONString("不支援的讀取型態【".$_POST["type"]."】", STATUS_CODE::UNSUPPORT_FAIL);
