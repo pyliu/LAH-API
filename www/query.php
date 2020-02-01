89526de9dc5909aab70254180674a8d3d7e2f800
pyliu <3634168+pyliu@users.noreply.github.com>
2020-02-01 17:29:04 +0800

    use common naming jquery.min.js for inclusion

--- www/query.php
+++ www/query.php
@@ -353,7 +353,7 @@ fieldset fieldset legend {
   </small>
   <!-- Bootstrap core JavaScript -->
   <!-- Placed at the end of the document so the pages load faster -->
-  <script src="assets/js/jquery-3.4.1.min.js"></script>
+  <script src="assets/js/jquery.min.js"></script>
   <script src="assets/js/popper.min.js"></script>
   <script src="assets/js/bootstrap.min.js"></script>
   <!-- bs datepicker -->
