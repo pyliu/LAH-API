e7ad470ce12d50077b9f3e75e9fa1cf407098469
pyliu <3634168+pyliu@users.noreply.github.com>
2020-02-02 10:41:48 +0800

    add schedule-task sub component

--- www/assets/js/components/watchdog.js
+++ www/assets/js/components/watchdog.js
@@ -1,32 +1,10 @@
 if (Vue) {
+    // for using countdown
     Vue.component(VueCountdown.name, VueCountdown);
     Vue.component("watchdog", {
         template: `<b-row>
             <b-col>
-                <b-card header="排程儀表版">
-                    <b-row>
-                        <b-col cols="8">
-                            <countdown ref="countdown" :time="timer_milliseconds" :auto-start="false">
-                                <template slot-scope="props">{{ props.minutes }}:{{ props.seconds }} 後自動執行</template>
-                            </countdown>
-                        </b-col>
-                        <b-col cols="4">
-                            <b-input-group size="sm">
-                                <b-input-group-prepend is-text>顯示個數</b-input-group-prepend>
-                                <b-form-input
-                                    type="number"
-                                    v-model="display_count"
-                                    size="sm"
-                                ></b-form-input>
-                            </b-input-group>
-                        </b-col>
-                    </b-row>
-                    <small>
-                        <b-list-group flush>
-                            <b-list-group-item v-for="item in schedule_history">{{item}}</b-list-group-item>
-                        </b-list-group>
-                    </small>
-                </b-card>
+                <schedule-task></schedule-task>
             </b-col>
             <b-col>
                 <b-card bo-body header="紀錄儀表版">
@@ -54,24 +32,6 @@ if (Vue) {
             }
         },
         methods: {
-            resetCountdown: function () {
-                this.$refs.countdown.totalMilliseconds = this.timer_milliseconds;
-                this.$refs.countdown.start();
-            },
-            startCountdown: function () {
-                this.$refs.countdown.start();
-            },
-            endCountdown: function () {
-                this.$refs.countdown.totalMilliseconds = 0;
-            },
-            addScheduleHistory: function (message) {
-                if (this.schedule_history.length == this.display_count) {
-                    this.schedule_history.pop();
-                } else if (this.schedule_history.length > this.display_count) {
-                    this.schedule_history = [];
-                }
-                this.schedule_history.unshift(message);
-            },
             addLogList: function (message) {
                 if (this.log_list.length == this.display_count) {
                     this.log_list.pop();
@@ -113,47 +73,105 @@ if (Vue) {
                     });
                     console.error("watchdog::callLogAPI parsing failed", ex);
                 });
-            },
-            callWatchdogAPI: function() {
-                this.endCountdown();
-                clearTimeout(this.watchdog_timer);
-
-                // generate current date time string
-                let dt = new Date();
-                let now = `${dt.getFullYear()}-${(dt.getMonth()+1).toString().padStart(2, '0')}-${(dt.getDate().toString().padStart(2, '0'))} ${dt.getHours().toString().padStart(2, '0')}:${dt.getMinutes().toString().padStart(2, '0')}:${dt.getSeconds().toString().padStart(2, '0')}`;
-                
-                let body = new FormData();
-                body.append("type", "watchdog");
-                asyncFetch("query_json_api.php", {
-                    method: "POST",
-                    body: body
-                }).then(jsonObj => {
-                    // normal success jsonObj.status == XHR_STATUS_CODE.SUCCESS_NORMAL
-                    if (jsonObj.status == XHR_STATUS_CODE.SUCCESS_NORMAL) {
-                        this.addScheduleHistory(`${now} 結果: OK`);
-                        // Backend will check if it needs to do or not
-                        this.watchdog_timer = setTimeout(this.callWatchdogAPI, this.timer_milliseconds);	// call the watchdog every 15 mins
-                        this.startCountdown();
-                    } else {
-                        // stop the timer if API tells it is not working
-                        this.addScheduleHistory(`${now} 結果: ${jsonObj.message}`);
-                        console.warn(jsonObj.message);
-                    }
-                }).catch(ex => {
-                    this.endCountdown();
-                    this.addScheduleHistory(`${now} 結果: ${ex.message}`);
-                    showAlert({
-                        title: 'watchdog::callWatchdogAPI parsing failed',
-                        message: ex.message,
-                        type: 'danger'
-                    });
-                    console.error("watchdog::callWatchdogAPI parsing failed", ex);
-                });
             }
         },
         mounted() {
-            this.callWatchdogAPI();
             this.callLogAPI();
+        },
+        components: {
+            "schedule-task": {
+                template: `<b-card header="排程儀表版">
+                    <b-row>
+                        <b-col cols="8">
+                            <countdown ref="countdown" :time="milliseconds" :auto-start="false">
+                                <template slot-scope="props">{{ props.minutes }}:{{ props.seconds }} 後自動執行</template>
+                            </countdown>
+                        </b-col>
+                        <b-col cols="4">
+                            <b-input-group size="sm">
+                                <b-input-group-prepend is-text>顯示個數</b-input-group-prepend>
+                                <b-form-input
+                                    type="number"
+                                    v-model="count"
+                                    size="sm"
+                                ></b-form-input>
+                            </b-input-group>
+                        </b-col>
+                    </b-row>
+                    <small>
+                        <b-list-group flush>
+                            <b-list-group-item v-for="item in history">{{item}}</b-list-group-item>
+                        </b-list-group>
+                    </small>
+                </b-card>`,
+                data: function() {
+                    return{
+                        milliseconds: 15 * 60 * 1000,
+                        count: 10,
+                        history: [],
+                        watchdog_timer: null
+                    }
+                },
+                methods: {
+                    resetCountdown: function () {
+                        this.$refs.countdown.totalMilliseconds = this.milliseconds;
+                        this.$refs.countdown.start();
+                    },
+                    startCountdown: function () {
+                        this.$refs.countdown.start();
+                    },
+                    endCountdown: function () {
+                        this.$refs.countdown.totalMilliseconds = 0;
+                    },
+                    addHistory: function (message) {
+                        if (this.history.length == this.count) {
+                            this.history.pop();
+                        } else if (this.history.length > this.count) {
+                            this.history = [];
+                        }
+                        this.history.unshift(message);
+                    },
+                    callWatchdogAPI: function() {
+                        this.endCountdown();
+                        clearTimeout(this.watchdog_timer);
+        
+                        // generate current date time string
+                        let dt = new Date();
+                        let now = `${dt.getFullYear()}-${(dt.getMonth()+1).toString().padStart(2, '0')}-${(dt.getDate().toString().padStart(2, '0'))} ${dt.getHours().toString().padStart(2, '0')}:${dt.getMinutes().toString().padStart(2, '0')}:${dt.getSeconds().toString().padStart(2, '0')}`;
+                        
+                        let body = new FormData();
+                        body.append("type", "watchdog");
+                        asyncFetch("query_json_api.php", {
+                            method: "POST",
+                            body: body
+                        }).then(jsonObj => {
+                            // normal success jsonObj.status == XHR_STATUS_CODE.SUCCESS_NORMAL
+                            if (jsonObj.status == XHR_STATUS_CODE.SUCCESS_NORMAL) {
+                                this.addHistory(`${now} 結果: OK`);
+                                // Backend will check if it needs to do or not
+                                this.watchdog_timer = setTimeout(this.callWatchdogAPI, this.milliseconds);	// call the watchdog every 15 mins
+                                this.startCountdown();
+                            } else {
+                                // stop the timer if API tells it is not working
+                                this.addHistory(`${now} 結果: ${jsonObj.message}`);
+                                console.warn(jsonObj.message);
+                            }
+                        }).catch(ex => {
+                            this.endCountdown();
+                            this.addHistory(`${now} 結果: ${ex.message}`);
+                            showAlert({
+                                title: 'schedule-task::callWatchdogAPI parsing failed',
+                                message: ex.message,
+                                type: 'danger'
+                            });
+                            console.error("schedule-task::callWatchdogAPI parsing failed", ex);
+                        });
+                    }
+                },
+                mounted() {
+                    this.callWatchdogAPI();
+                }
+            }
         }
     });
 } else {
