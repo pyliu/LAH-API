8488d06d9033270918ff9408835ba9bb2b4c76a7
pyliu <3634168+pyliu@users.noreply.github.com>
2020-02-25 22:51:48 +0800

    use fa-th icon

--- www/assets/js/components/lah-vue.js
+++ www/assets/js/components/lah-vue.js
@@ -231,7 +231,7 @@ Vue.component("lah-header", {
         links: [{
             text: "儀錶板",
             url: ["index.html", "/"],
-            icon: "fa-columns",
+            icon: "fa-th",
             need_admin: true
         }, {
             text: "案件追蹤",
