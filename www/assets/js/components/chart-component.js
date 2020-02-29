e5ce6470c6da74211975d2f8d748d5087909a89a
pyliu <3634168+pyliu@users.noreply.github.com>
2020-02-29 12:40:24 +0800

    handle chart click event at parent

--- www/assets/js/components/chart-component.js
+++ www/assets/js/components/chart-component.js
@@ -123,12 +123,12 @@ if (Vue) {
                         title: { display: false, text: "自訂標題", position: "bottom" },
                         onClick: function(e) {
                             let payload = {};
-                            payload["point"] = myChart.getElementAtEvent(evt)[0];
+                            payload["point"] = this.inst.getElementAtEvent(evt)[0];
                             if (payload["point"]) {
                                 payload["label"] = myChart.data.labels[firstPoint._index];
                                 payload["value"] = myChart.data.datasets[firstPoint._datasetIndex].data[firstPoint._index];
                             }
-                            // parent use a handle function to catch the event, e.g. catchClick(e, payload) { ... }
+                            // parent uses a handle function to catch the event, e.g. catchClick(e, payload) { ... }
                             this.$emit("click", e, payload);
                         }
                     }, opts)
