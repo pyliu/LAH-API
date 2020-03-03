4d758ae6300b794ee413e37678e708cdf127707a
pyliu <3634168+pyliu@users.noreply.github.com>
2020-03-03 16:39:25 +0800

    add select/update/insert/delete operations

--- www/include/SQLiteDB.class.php
+++ www/include/SQLiteDB.class.php
@@ -3,6 +3,7 @@ require_once("init.php");
 
 class SQLiteDB extends SQLite3 {
     private $filepath;
+    private $last_ret;
 
     private function createStatsTable() {
         global $log;
@@ -29,6 +30,56 @@ class SQLiteDB extends SQLite3 {
 
     function __destruct() {
         $this->close();
+    }
+
+    public function select($sql, $only_one = false) {
+        global $log;
+        $log->info(__METHOD__.": ".$sql." 只取一筆: $only_one");
+        $this->last_ret = $this->query($sql);
+        if ($only_one) {
+            return $this->last_ret->fetchArray(SQLITE3_ASSOC);
+        }
+        $ret = array();
+        while ($row = $this->last_ret->fetchArray(SQLITE3_ASSOC)) {
+            $ret[] = $row;
+        }
+        return $ret;
+    }
+
+    public function insert($sql) {
+        global $log;
+        $log->info(__METHOD__.": ".$sql);
+        $ret = $this->exec($sql);
+        if(!$ret){
+            $log->error($this->lastErrorMsg());
+            return false;
+        }
+        $log->info(__METHOD__.": 插入資料成功。");
+        return true;
+    }
+
+    public function update($sql) {
+        global $log;
+        $log->info(__METHOD__.": ".$sql);
+        $ret = $this->exec($sql);
+        if(!$ret){
+            $log->error($this->lastErrorMsg());
+            return false;
+        }
+        $log->info(__METHOD__.": 更新資料成功。 (".$this->changes()."筆)");
+        return $this->changes();
+    }
+
+    public function delete($sql) {
+        global $log;
+        $log->info(__METHOD__.": ".$sql);
+        $ret = $this->exec($sql);
+        if(!$ret){
+            $log->error($this->lastErrorMsg());
+            return false;
+        }
+        $log->info(__METHOD__.": 刪除資料成功。 (".$this->changes()."筆)");
+        return $this->changes();
     }
 }
 ?>
