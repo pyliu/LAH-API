bd64d196bdb9ac2b43902d3aed7cb9c51f138d75
pyliu <3634168+pyliu@users.noreply.github.com>
2020-03-10 10:03:59 +0800

    add template files

---
+++ www/assets/js/components/lah-template.js
@@ -0,0 +1,13 @@
+if (Vue) {
+    Vue.component('lah-xxxxxxxx', {
+        template: ``,
+        data: () => { return {
+            
+        } },
+        methods: {},
+        created() {},
+        mounted() {}
+    });
+} else {
+    console.error("vue.js not ready ... lah-xxxxxxxx component can not be loaded.");
+}
