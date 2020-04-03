be388472401a7ebefb03f30b53ca7f738e70eb4f
pyliu <3634168+pyliu@users.noreply.github.com>
2020-04-03 20:14:53 +0800

    to html

--- www/watch_dog_task.php
+++ www/watch_dog_task.php
@@ -20,7 +20,7 @@ if (!empty($rows)) {
     }
     
     $msg = new Message();
-    $content = "系統目前找到下列跨所註記遺失案件:\r\n\r\n".implode("\r\n", $case_ids)."\r\n\r\n請前往 http://$host_ip/watchdog.php 修正。";
+    $content = "系統目前找到下列跨所註記遺失案件:\r\n\r\n".implode("\r\n", $case_ids)."\r\n\r\n請前往 http://$host_ip/watchdog.html 修正。";
     foreach (SYSTEM_CONFIG['ADM_IPS'] as $adm_ip) {
         if ($adm_ip == '::1') {
             continue;
