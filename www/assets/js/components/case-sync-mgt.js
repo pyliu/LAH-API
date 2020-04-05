90fe0d06c9ce54df94ad1ef902476aa80996c605
pyliu <3634168+pyliu@users.noreply.github.com>
2020-04-05 16:02:40 +0800

    case-sync-mgt uses v-model for the case id

--- www/assets/js/components/case-sync-mgt.js
+++ www/assets/js/components/case-sync-mgt.js
@@ -4,7 +4,7 @@ if (Vue) {
             <legend>同步案件</legend>
             <b-form-row class="mb-2">
                 <b-col>
-                    <case-input-group-ui @update="handleUpdate" @enter="check" type="sync" prefix="case_sync"></case-input-group-ui>
+                    <case-input-group-ui v-model="id" @enter="check" type="sync" prefix="case_sync"></case-input-group-ui>
                 </b-col>
             </b-form-row>
             <b-form-row>
@@ -18,157 +18,129 @@ if (Vue) {
         </fieldset>`,
         data: () => {
             return {
-                year: "108",
-                code: "HB04",
-                num: "000010"
+                year: undefined,
+                code: undefined,
+                num: undefined,
+                id: undefined
             }
         },
-        methods: {
-            handleUpdate: function(e, data) {
-                this.year = data.year;
-                this.code = data.code;
-                this.num = data.num;
-            },
-            validate: function() {
-                let year = this.year;
-                let code = this.code;
-                let num = this.num;
-                if (this.empty(year)) {
-                    addNotification({
-                        title: "查詢遠端案件資料",
-                        subtitle: `${year}-${code}-${num}`,
-                        message: "請重新選擇「年」欄位!",
-                        type: "warning"
-                    });
+        computed: {
+            validate() {
+                this.year = this.id.substring(0, 3);
+                this.code = this.id.substring(3, 7);
+                this.num = this.id.substring(7);
+                let regex = /^[0-9]{3}$/i;
+                if (!regex.test(this.year)) {
+                    this.$warn("year format is not valid.", this.year, this.id);
                     return false;
                 }
-                if (this.empty(code)) {
-                    addNotification({
-                        title: "查詢遠端案件資料",
-                        subtitle: `${year}-${code}-${num}`,
-                        message: "請重新選擇「字」欄位!",
-                        type: "warning"
-                    });
+                regex = /^H[A-Z0-9]{3}$/i;
+                if (!regex.test(this.code)) {
+                    this.$warn("code format is not valid.", this.code, this.id);
                     return false;
                 }
-                let number = num.replace(/\D/g, "");
-                let offset = 6 - number.length;
-                if (this.empty(number) || isNaN(number) || offset < 0) {
-                    addNotification({
-                        title: "查詢遠端案件資料",
-                        subtitle: `${year}-${code}-${number}`,
-                        message: `「號」格式有問題，請查明修正【目前：${num}】`,
-                        type: "danger"
-                    });
+                let number = parseInt(this.num);
+                if (this.empty(number) || isNaN(number)) {
+                    this.$warn("number is empty or NaN!", this.num, this.id);
                     return false;
                 }
                 return true;
-            },
+            }
+        },
+        methods: {
             check: function(e) {
-                if (!this.validate()) {
-                    showAlert({message: `輸入資料格式有誤，無法查詢 ${this.year}-${this.code}-${this.num}`, type: "warning"});
-                    return false;
-                }
-                
-                let year = this.year;
-                let code = this.code;
-                let number = this.num;
-                
-                // toggle button disable attr
-                this.isBusy = true;
-            
-                let offset = 6 - number.length;
-                if (offset > 0) {
-                    // padding leading zero for the number
-                    number = number.padStart(6, "0");
-                }
-
-                // prepare post params
-                let id = trim(year + code + number);
-                this.$http.post(CONFIG.JSON_API_EP, {
-                    type: "diff_xcase",
-                    id: id
-                }).then(res => {
-                    let html = "<div>案件詳情：<a href='javascript:void(0)' id='sync_x_case_serial'>" + year + "-" + code + "-" + number + "</a><div>";
-                    if (res.data.status == XHR_STATUS_CODE.SUCCESS_NORMAL) {
-                        html += "<i class='fas fa-circle text-warning'></i>&ensp;請參考下列資訊： <button id='sync_x_case_confirm_button' class='btn btn-sm btn-success' title='同步全部欄位'>同步</button>";
-                        html += "<table class='table table-hover b-table table-striped table-bordered table-sm text-center mt-1'>";
-                        html += "<tr class='bg-dark text-white forn-weight-bold'><th>欄位名稱</th><th>欄位代碼</th><th>局端</th><th>本所</th><th>單欄同步</th></tr>";
-                        for (let key in res.data.raw) {
-                            html += "<tr>";
-                            html += "<td>" + res.data.raw[key]["TEXT"] + "</td>";
-                            html += "<td>" + res.data.raw[key]["COLUMN"] + "</td>";
-                            html += "<td class='text-danger'>" + res.data.raw[key]["REMOTE"] + "</td>";
-                            html += "<td class='text-info'>" + res.data.raw[key]["LOCAL"] + "</td>";
-                            html += "<td><button id='sync_column_" + res.data.raw[key]["COLUMN"] + "' data-column='" + res.data.raw[key]["COLUMN"] + "' class='btn btn-sm btn-outline-dark sync_column_button'>同步" + res.data.raw[key]["COLUMN"] + "</button></td>";
-                            html += "</tr>";
-                        };
-                        html += "</table>";
-                        showModal({
-                            title: "案件比對詳情",
-                            body: html,
-                            callback: () => {
-                                $("#sync_x_case_confirm_button").off("click").on("click", this.syncWholeCase.bind(this, id));
-                                let that = this;
-                                $(".sync_column_button").off("click").each((idx, element) => {
-                                    let column = $(element).data("column");
-                                    $(element).on("click", that.syncCaseColumn.bind(that, id, column));
-                                });
-                                $("#sync_x_case_serial").off("click").on("click", function(e) {
-                                    window.vueApp.fetchRegCase(e)
-                                });
-                            },
-                            size: "lg"
-                        });
-                    } else if (res.data.status == XHR_STATUS_CODE.FAIL_WITH_LOCAL_NO_RECORD) {
-                        showModal({
-                            title: "本地端無資料",
-                            body: `<div>
-                                <i class='fas fa-circle text-warning'></i>&ensp;
-                                ${res.data.message}
-                                <button id='inst_x_case_confirm_button'>新增本地端資料</button>
-                            </div>`,
-                            callback: () => {
-                                $("#sync_x_case_serial").off("click").on("click", window.vueApp.fetchRegCase);
-                                $("#inst_x_case_confirm_button").off("click").on("click", this.instRemoteCase.bind(this, id));
-                            },
-                            size: "md"
-                        });
-                    } else if (res.data.status == XHR_STATUS_CODE.FAIL_WITH_REMOTE_NO_RECORD) {
-                        html += "<div><i class='fas fa-circle text-secondary'></i>&ensp;" + res.data.message + "</div>";
-                        addNotification({
-                            title: "查詢遠端案件資料",
-                            subtitle: `${year}-${code}-${number}`,
-                            message: html,
-                            type: "warning"
-                        });
-                    } else {
-                        html += "<div><i class='fas fa-circle text-success'></i>&ensp;" + res.data.message + "</div>";
-                        addNotification({
-                            title: "查詢遠端案件資料",
-                            subtitle: `${year}-${code}-${number}`,
-                            message: html,
-                            type: "success",
-                            callback: () => $("#sync_x_case_serial").off("click").on("click", function(e) {
-                                window.vueApp.fetchRegCase(e);
-                            })
-                        });
-                    }
-                }).catch(err => {
-                    // remove the fieldset since the function is not working ... 
-                    let fieldset = $("#case-sync-mgt-fieldset");
-                    let container = fieldset.closest("div.col-6");
-                    addAnimatedCSS(fieldset, {
-                        name: ANIMATED_TRANSITIONS[rand(ANIMATED_TRANSITIONS.length)].out,
-                        callback: () => {
-                            fieldset.remove();
-                            container.append(jQuery.parseHTML('<i class="ld ld-breath fas fa-ban text-danger fa-3x"></i>')).addClass("my-auto text-center");
+                if (this.validate) {
+                    this.isBusy = true;
+                    this.$http.post(CONFIG.JSON_API_EP, {
+                        type: "diff_xcase",
+                        id: this.id
+                    }).then(res => {
+                        let html = "<div>案件詳情：<a href='javascript:void(0)' id='sync_x_case_serial'>" + this.year + '-' + this.code + '-' + this.num + "</a><div>";
+                        if (res.data.status == XHR_STATUS_CODE.SUCCESS_NORMAL) {
+                            html += "<i class='fas fa-circle text-warning'></i>&ensp;請參考下列資訊： <button id='sync_x_case_confirm_button' class='btn btn-sm btn-success' title='同步全部欄位'>同步</button>";
+                            html += "<table class='table table-hover b-table table-striped table-bordered table-sm text-center mt-1'>";
+                            html += "<tr class='bg-dark text-white forn-weight-bold'><th>欄位名稱</th><th>欄位代碼</th><th>局端</th><th>本所</th><th>單欄同步</th></tr>";
+                            for (let key in res.data.raw) {
+                                html += "<tr>";
+                                html += "<td>" + res.data.raw[key]["TEXT"] + "</td>";
+                                html += "<td>" + res.data.raw[key]["COLUMN"] + "</td>";
+                                html += "<td class='text-danger'>" + res.data.raw[key]["REMOTE"] + "</td>";
+                                html += "<td class='text-info'>" + res.data.raw[key]["LOCAL"] + "</td>";
+                                html += "<td><button id='sync_column_" + res.data.raw[key]["COLUMN"] + "' data-column='" + res.data.raw[key]["COLUMN"] + "' class='btn btn-sm btn-outline-dark sync_column_button'>同步" + res.data.raw[key]["COLUMN"] + "</button></td>";
+                                html += "</tr>";
+                            };
+                            html += "</table>";
+                            showModal({
+                                title: "案件比對詳情",
+                                body: html,
+                                callback: () => {
+                                    $("#sync_x_case_confirm_button").off("click").on("click", this.syncWholeCase.bind(this, this.id));
+                                    $(".sync_column_button").off("click").each((idx, element) => {
+                                        let column = $(element).data("column");
+                                        $(element).on("click", this.syncCaseColumn.bind(this, this.id, column));
+                                    });
+                                    $("#sync_x_case_serial").off("click").on("click", function(e) {
+                                        window.vueApp.fetchRegCase(e)
+                                    });
+                                },
+                                size: "lg"
+                            });
+                        } else if (res.data.status == XHR_STATUS_CODE.FAIL_WITH_LOCAL_NO_RECORD) {
+                            showModal({
+                                title: "本地端無資料",
+                                body: `<div>
+                                    <i class='fas fa-circle text-warning'></i>&ensp;
+                                    ${res.data.message}
+                                    <button id='inst_x_case_confirm_button'>新增本地端資料</button>
+                                </div>`,
+                                callback: () => {
+                                    $("#sync_x_case_serial").off("click").on("click", window.vueApp.fetchRegCase);
+                                    $("#inst_x_case_confirm_button").off("click").on("click", this.instRemoteCase.bind(this, this.id));
+                                },
+                                size: "md"
+                            });
+                        } else if (res.data.status == XHR_STATUS_CODE.FAIL_WITH_REMOTE_NO_RECORD) {
+                            html += "<div><i class='fas fa-circle text-secondary'></i>&ensp;" + res.data.message + "</div>";
+                            addNotification({
+                                title: "查詢遠端案件資料",
+                                subtitle: `${this.year}-${this.code}-${this.num}`,
+                                message: html,
+                                type: "warning"
+                            });
+                        } else {
+                            html += "<div><i class='fas fa-circle text-success'></i>&ensp;" + res.data.message + "</div>";
+                            addNotification({
+                                title: "查詢遠端案件資料",
+                                subtitle: `${this.year}-${this.code}-${this.num}`,
+                                message: html,
+                                type: "success",
+                                callback: () => $("#sync_x_case_serial").off("click").on("click", function(e) {
+                                    window.vueApp.fetchRegCase(e);
+                                })
+                            });
                         }
+                    }).catch(err => {
+                        // remove the fieldset since the function is not working ... 
+                        let fieldset = $("#case-sync-mgt-fieldset");
+                        let container = fieldset.closest("div.col-6");
+                        addAnimatedCSS(fieldset, {
+                            name: ANIMATED_TRANSITIONS[rand(ANIMATED_TRANSITIONS.length)].out,
+                            callback: () => {
+                                fieldset.remove();
+                                container.append(jQuery.parseHTML('<i class="ld ld-breath fas fa-ban text-danger fa-3x"></i>')).addClass("my-auto text-center");
+                            }
+                        });
+                        this.error = err;
+                    }).finally(() => {
+                        this.isBusy = false;
                     });
-                    this.error = err;
-                }).finally(() => {
-                    this.isBusy = false;
-                });
+                } else {
+                    this.$alert({
+                        title: '案件同步查詢',
+                        message: `案件ID有問題，請檢查後再重試！ (${this.year}-${this.code}-${this.num})`,
+                        variant: 'warning'
+                    });
+                }
             },
             syncCaseColumn: function(id, column) {
                 showConfirm(`確定要同步${column}？`, () => {
