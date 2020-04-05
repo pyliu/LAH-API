637743f3bc486da4ef04a7ebaa028e35719e33ef
pyliu <3634168+pyliu@users.noreply.github.com>
2020-04-05 15:08:04 +0800

    use v-model with id in case-state-mgt

--- www/assets/js/components/case-state-mgt.js
+++ www/assets/js/components/case-state-mgt.js
@@ -4,7 +4,7 @@ if (Vue) {
             <legend>案件狀態</legend>
             <b-form-row class="mb-2">
                 <b-col>
-                    <case-input-group-ui v-model="id" @update="handleUpdate" @enter="query" type="reg" prefix="case_state"></case-input-group-ui>
+                    <case-input-group-ui v-model="id" @enter="query" type="reg" prefix="case_state"></case-input-group-ui>
                 </b-col>
             </b-form-row>
             <b-form-row>
@@ -18,66 +18,75 @@ if (Vue) {
         </fieldset>`,
         data: () => {
             return {
-                year: "108",
-                code: "HB04",
-                num: "000010",
                 dialog: null,
                 id: undefined
             }
         },
-        methods: {
-            handleUpdate: function(e, data) {
-                this.year = data.year;
-                this.code = data.code;
-                this.num = data.num;
-            },
-            query: function(e) {
-                let data = {year: this.year, code: this.code, num: this.num};
-                if (!window.vueApp.checkCaseUIData(data)) {
-                    addNotification({
-                        title: "案件查詢",
-                        subtitle: `${data.year}-${data.code}-${data.num}`,
-                        message: `輸入資料格式有誤，無法查詢。`,
-                        type: "warning"});
+        computed: {
+            validate() {
+                let year = this.id.substring(0, 3);
+                let code = this.id.substring(3, 7);
+                let num = this.id.substring(7);
+                if (this.empty(year)) {
+                    this.$warn(this.id, "year is empty!");
                     return false;
                 }
-
-                // prepare post params
-                let id = trim(`${this.year}${this.code}${this.num}`);
-                
-                this.isBusy = true;
-                this.$http.post(CONFIG.JSON_API_EP, {
-                    type: "reg_case",
-                    id: id
-                }).then(res => {
-                    if (res.data.status == XHR_STATUS_CODE.DEFAULT_FAIL) {
-                        addNotification({
-                            title: "案件查詢",
-                            subtitle: id,
-                            message: res.data.message,
-                            type: "warning"
-                        });
-                    } else if (res.data.status == XHR_STATUS_CODE.UNSUPPORT_FAIL) {
-                        throw new Error("查詢失敗：" + res.data.message);
-                    } else {
-                        // create sub-component dynamically
-                        let v = this.$createElement("lah-reg-case-state-mgt", {
-                            props: {
-                                bakedData: res.data.baked,
-                                progress: true
-                            }
-                        })
-                        showModal({
-                            title: "調整登記案件欄位資料",
-                            body: v,
-                            size: "md"
-                        });
-                    }
-                }).catch(err => {
-                    this.error = err;
-                }).finally(() => {
-                    this.isBusy = false;
-                });
+                if (this.empty(code)) {
+                    this.$warn(this.id, "code is empty!");
+                    return false;
+                }
+                if (this.empty(num) || isNaN(num)) {
+                    this.$warn(this.id, "code is empty or NaN!");
+                    return false;
+                }
+                return true;
+            }
+        },
+        methods: {
+            query: function(e) {
+                if (this.validate) {
+                    // prepare post params
+                    let id = this.id;
+                    this.isBusy = true;
+                    this.$http.post(CONFIG.JSON_API_EP, {
+                        type: "reg_case",
+                        id: id
+                    }).then(res => {
+                        if (res.data.status == XHR_STATUS_CODE.DEFAULT_FAIL) {
+                            addNotification({
+                                title: "案件查詢",
+                                subtitle: id,
+                                message: res.data.message,
+                                type: "warning"
+                            });
+                        } else if (res.data.status == XHR_STATUS_CODE.UNSUPPORT_FAIL) {
+                            throw new Error("查詢失敗：" + res.data.message);
+                        } else {
+                            // create sub-component dynamically
+                            let v = this.$createElement("lah-reg-case-state-mgt", {
+                                props: {
+                                    bakedData: res.data.baked,
+                                    progress: true
+                                }
+                            })
+                            showModal({
+                                title: "調整登記案件欄位資料",
+                                body: v,
+                                size: "md"
+                            });
+                        }
+                    }).catch(err => {
+                        this.error = err;
+                    }).finally(() => {
+                        this.isBusy = false;
+                    });
+                } else {
+                    this.$alert({
+                        title: '案件狀態查詢',
+                        message: `案件ID有問題，請檢查後再重試！ (${this.id})`,
+                        variant: 'warning'
+                    });
+                }
             },
             popup: () => {
                 showModal({
