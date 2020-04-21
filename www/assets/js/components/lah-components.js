a2d1ddca4fd2d40a7a7b1613a2157f9925223a60
pyliu <3634168+pyliu@users.noreply.github.com>
2020-04-21 20:30:14 +0800

    wording

--- www/assets/js/components/lah-components.js
+++ www/assets/js/components/lah-components.js
@@ -851,14 +851,10 @@ if (Vue) {
                 });
             },
             popup() {
-                let todo = this.$createElement(
-                    'p',
-                    { props: { icon: 'search', variant: 'primary', size: 'lg' } },
-                    [ ' 輸入 使用者代碼(如：HB0544) 或 名稱(如：奕) 或 電腦IP位址(如：220.1.35.48) 來查找。' ]
-                );
                 this.msgbox({
                     title: '<i class="fa fa-search fa-lg"></i> 使用者搜尋說明',
-                    message: todo
+                    message: `輸入下列條件來查找。 <ul><li>使用者代碼(如：HB0544)</li> <li>名稱(如：奕)</li> <li>電腦IP位址(如：220.1.35.48)</li> </ul>`,
+                    size: "sm"
                 });
             }
         },
