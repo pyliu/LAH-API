73e01046cc2044ea3433ed2a5db204aa9638dd7b
pyliu <3634168+pyliu@users.noreply.github.com>
2020-08-12 19:24:57 +0800

    add FileAPIExcelExportCommand

--- www/include/api/FileAPICommandFactory.class.php
+++ www/include/api/FileAPICommandFactory.class.php
@@ -4,6 +4,7 @@ require_once("FileAPINotSupportCommand.class.php");
 require_once("FileAPISQLCsvCommand.class.php");
 require_once("FileAPISQLTxtCommand.class.php");
 require_once("FileAPILogExportCommand.class.php");
+require_once("FileAPIExcelExportCommand.class.php");
 
 abstract class FileAPICommandFactory {
     public static function getCommand($type) {
@@ -13,20 +14,19 @@ abstract class FileAPICommandFactory {
                 $log->info("輸出CSV檔案");
                 $log->info($_POST["sql"]);
                 return new FileAPISQLCsvCommand($_POST["sql"]);
-                break;
             case "file_sql_txt":
                 $log->info("輸出TXT檔案");
                 $log->info($_POST["sql"]);
                 return new FileAPISQLTxtCommand($_POST["sql"]);
-                break;
             case "file_log":
                 $log->info("輸出LOG檔案");
                 $log->info($_POST["date"]);
                 return new FileAPILogExportCommand($_POST["date"]);
-                break;
+            case "file_xlsx_test":
+                $log->info("輸出XLSX測試檔案");
+                return new FileAPIExcelExportCommand();
             default:
                 return new FileAPINotSupportCommand();
-                break;
         }
     }
 }
--- www/include/api/FileAPIExcelExportCommand.class.php
+++ www/include/api/FileAPIExcelExportCommand.class.php
@@ -1,19 +1,16 @@
 <?php
 require_once("FileAPICommand.class.php");
-require_once(dirname(dirname(dirname(__FILE__))).'vendor/autoload.php');
+require_once(dirname(dirname(dirname(__FILE__))).'/vendor/autoload.php');
 use PhpOffice\PhpSpreadsheet\Spreadsheet;
-use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
+// use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
 use PhpOffice\PhpSpreadsheet\IOFactory;
 
-class FileAPIExportExcelCommand extends FileAPICommand {
-    private $sql;
-    function __construct($sql) {
-        $this->sql = $sql;
-    }
+class FileAPIExcelExportCommand extends FileAPICommand {
+    function __construct() {}
 
     function __destruct() {}
 
-    private function outputXlsx() {
+    private function outputXlsx($filename = 'download') {
 
         // $spreadsheet = IOFactory::load('test.xlsx');
         // $worksheet = $spreadsheet->getActiveSheet();
@@ -25,12 +22,11 @@ class FileAPIExportExcelCommand extends FileAPICommand {
 
         // $writer = new Xlsx($spreadsheet);
         // $writer->save('exports/hello world.xlsx');
-        //header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
         header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
-        header('Content-Disposition: attachment;filename="檔名.xlsx"');
+        header('Content-Disposition: attachment;filename="'.$filename.'.xlsx"');
         header('Cache-Control: max-age=0');
 
-        $writer = \PhpOffice\PhpSpreadsheet\IOFactory::createWriter($spreadsheet, 'Xlsx');
+        $writer = IOFactory::createWriter($spreadsheet, 'Xlsx');
         $writer->save('php://output');
     }
 
