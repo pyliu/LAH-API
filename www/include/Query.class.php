e2e9e0d3f3952c8c76177cf65e6e239b40288bd7
pyliu <3634168+pyliu@users.noreply.github.com>
2020-08-26 11:23:56 +0800

    use iconv to make chinese mapping big5

--- www/include/Query.class.php
+++ www/include/Query.class.php
@@ -123,8 +123,61 @@ class Query {
 				r.SR_SUBAGENT_NAME AS \"複代理人姓名\",
 				r.SR08             AS \"LOG時間\",
 				r.SR_METHOD        AS \"申請方式\",
-				t.MD04 AS \"謄本種類\",
-				t.MD05 AS \"謄本項目\"
+				(CASE
+					WHEN t.MD04 = 'A' THEN '".iconv("utf-8", "big5", "登記電子資料謄本")."'
+					WHEN t.MD04 = 'B' THEN '".iconv("utf-8", "big5", "地價電子資料謄本")."'
+					WHEN t.MD04 = 'C' THEN '".iconv("utf-8", "big5", "地籍圖謄本")."'
+					WHEN t.MD04 = 'D' THEN '".iconv("utf-8", "big5", "建物平面圖謄本")."'
+					WHEN t.MD04 = 'E' THEN '".iconv("utf-8", "big5", "人工登記簿謄本")."'
+					WHEN t.MD04 = 'F' THEN '".iconv("utf-8", "big5", "閱覽")."'
+					WHEN t.MD04 = 'G' THEN '".iconv("utf-8", "big5", "列印電子資料")."'
+					WHEN t.MD04 = 'H' THEN '".iconv("utf-8", "big5", "申請")."'
+					WHEN t.MD04 = 'I' THEN '".iconv("utf-8", "big5", "測量")."'
+					WHEN t.MD04 = 'J' THEN '".iconv("utf-8", "big5", "異動索引")."'
+					WHEN t.MD04 = 'K' THEN '".iconv("utf-8", "big5", "土地建物異動清冊")."'
+					WHEN t.MD04 = 'L' THEN '".iconv("utf-8", "big5", "代理人申請登記案件明細表")."'
+					WHEN t.MD04 = 'M' THEN '".iconv("utf-8", "big5", "土地參考資訊")."'
+					ELSE t.MD04
+       			END) AS \"謄本種類\",
+       			(CASE
+					WHEN t.MD05 = 'A' THEN '".iconv("utf-8", "big5", "全部")."'
+					WHEN t.MD05 = 'B' THEN '".iconv("utf-8", "big5", "所有權個人全部")."'
+					WHEN t.MD05 = 'C' THEN '".iconv("utf-8", "big5", "標示部")."'
+					WHEN t.MD05 = 'D' THEN '".iconv("utf-8", "big5", "所有權部")."'
+					WHEN t.MD05 = 'E' THEN '".iconv("utf-8", "big5", "他項權利部")."'
+					WHEN t.MD05 = 'F' THEN '".iconv("utf-8", "big5", "標示部及所有權部")."'
+					WHEN t.MD05 = 'G' THEN '".iconv("utf-8", "big5", "標示部及他項權利部")."'
+					WHEN t.MD05 = 'H' THEN '".iconv("utf-8", "big5", "他項權利部之個人")."'
+					WHEN t.MD05 = 'I' THEN '".iconv("utf-8", "big5", "他項權利個人全部")."'
+					WHEN t.MD05 = 'J' THEN '".iconv("utf-8", "big5", "標示部")."'
+					WHEN t.MD05 = 'K' THEN '".iconv("utf-8", "big5", "所有權部")."'
+					WHEN t.MD05 = 'KA' THEN '".iconv("utf-8", "big5", "收件年期字號")."'
+					WHEN t.MD05 = 'KB' THEN '".iconv("utf-8", "big5", "收件年字號+登序")."'
+					WHEN t.MD05 = 'KC' THEN '".iconv("utf-8", "big5", "收件年字號+統編")."'
+					WHEN t.MD05 = 'KD' THEN '".iconv("utf-8", "big5", "收件年字號+姓名")."'
+					WHEN t.MD05 = 'L' THEN '".iconv("utf-8", "big5", "標示部及所有權部")."'
+					WHEN t.MD05 = 'LA' THEN '".iconv("utf-8", "big5", "統計起始日期+統計終止日期+代理人統一編號")."'
+					WHEN t.MD05 = 'LB' THEN '".iconv("utf-8", "big5", "統計起始日期+統計終止日期")."'
+					WHEN t.MD05 = 'LC' THEN '".iconv("utf-8", "big5", "代理人統一編號")."'
+					WHEN t.MD05 = 'M' THEN '".iconv("utf-8", "big5", "標示部(標示部不存在)")."'
+					WHEN t.MD05 = 'N' THEN '".iconv("utf-8", "big5", "所有權部(標示部不存在)")."'
+					WHEN t.MD05 = 'O' THEN '".iconv("utf-8", "big5", "標示部及所有權部(標示部不存在)")."'
+					WHEN t.MD05 = 'P' THEN '".iconv("utf-8", "big5", "資料庫不存在")."'
+					WHEN t.MD05 = 'Q' THEN '".iconv("utf-8", "big5", "地籍圖謄本")."'
+					WHEN t.MD05 = 'R' THEN '".iconv("utf-8", "big5", "建物平面圖謄本")."'
+					WHEN t.MD05 = 'S' THEN '".iconv("utf-8", "big5", "登記簿謄本")."'
+					WHEN t.MD05 = 'T' THEN '".iconv("utf-8", "big5", "閱覽")."'
+					WHEN t.MD05 = 'U' THEN '".iconv("utf-8", "big5", "列印電子資料")."'
+					WHEN t.MD05 = 'V' THEN '".iconv("utf-8", "big5", "申請")."'
+					WHEN t.MD05 = 'W' THEN '".iconv("utf-8", "big5", "測量")."'
+					WHEN t.MD05 = 'X' THEN '".iconv("utf-8", "big5", "異動索引")."'
+					WHEN t.MD05 = 'XA' THEN '".iconv("utf-8", "big5", "收件年期字號")."'
+					WHEN t.MD05 = 'XB' THEN '".iconv("utf-8", "big5", "地建號")."'
+					WHEN t.MD05 = 'XC' THEN '".iconv("utf-8", "big5", "地建號+登序")."'
+					WHEN t.MD05 = 'XD' THEN '".iconv("utf-8", "big5", "地建號+部別")."'
+					WHEN t.MD05 = 'XE' THEN '".iconv("utf-8", "big5", "地建號+統編")."'
+					ELSE t.MD05
+       			END) AS \"謄本項目\"
 			FROM MOICAS.CUSMD2 t
 			INNER JOIN MOICAS.RSCNRL r
 				ON (t.MD03 = r.SR03)
