e6da4f31f863f914c411a313aa0c6bc8b2e88125
pyliu <3634168+pyliu@users.noreply.github.com>
2020-08-29 17:14:33 +0800

    use my open function to open external download link

--- www/assets/js/components/dashboards/lah-court-log-search.js
+++ www/assets/js/components/dashboards/lah-court-log-search.js
@@ -127,8 +127,10 @@ if (Vue) {
           numbers: this.format()
         }).then(res => {
           if (res.data.status == XHR_STATUS_CODE.SUCCESS_NORMAL) {
-            let w = window.open(CONFIG.API.FILE.XLSX);
-            setTimeout(() => w.document.close(), 1000);
+            this.open(CONFIG.API.FILE.XLSX, {target:{title:'下載XLSX'}});
+            // let w = window.open(CONFIG.API.FILE.XLSX);
+            // setTimeout(() => w.document.close(), 1000);
+            setTimeout(closeModal, 1000);
             this.notify({ title: '匯出EXCEL檔案', message: '<i class="fas fa-check"> 完成</i>', type: "success" });
           } else {
             let err = this.responseMessage(res.data.status);
