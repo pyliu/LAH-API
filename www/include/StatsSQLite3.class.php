9ed098826424e05aef2d2ac28e686ac74a9b7a7a
pyliu <3634168+pyliu@users.noreply.github.com>
2020-09-04 15:59:47 +0800

    too many logs

--- www/include/StatsSQLite3.class.php
+++ www/include/StatsSQLite3.class.php
@@ -129,8 +129,8 @@ class StatsSQLite3 {
         $stm->bindParam(':time', $ten_mins_ago);
         $ret = $stm->execute();
         if ($ret) {
-            $deleted_count = $this->db->changes();
-            if ($deleted_count > 0) $log->info("成功刪除 $deleted_count 筆紀錄(10分鐘前)。");
+            // $deleted_count = $this->db->changes();
+            // if ($deleted_count > 0) $log->info("成功刪除過期 $deleted_count 筆 ap_connection 統計紀錄。");
         } else {
             $log->error(__METHOD__.": 移除10分鐘前資料失敗【".$ten_mins_ago.", ".$this->db->lastErrorMsg()."】");
         }
