2fc5a83e71ae3c699f205a7fc5370e4e3b2349ce
pyliu <3634168+pyliu@users.noreply.github.com>
2020-09-05 13:34:10 +0800

    use isOfficeHours to check if reload is needed

--- www/assets/js/components/dashboards/lah-xap-connection-chart.js
+++ www/assets/js/components/dashboards/lah-xap-connection-chart.js
@@ -29,45 +29,50 @@ if (Vue) {
         },
         methods: {
             reload() {
-                this.isBusy = true;
-                this.$http.post(CONFIG.API.JSON.STATS, {
-                    type: "stats_ap_conn_latest",
-                    ip: this.ip,
-                    count: 11
-                }).then(res => {
-                    console.assert(res.data.status == XHR_STATUS_CODE.SUCCESS_NORMAL, `取得AP連線數回傳狀態碼有問題【${res.data.status}】`);
-                    if (res.data.status == XHR_STATUS_CODE.SUCCESS_NORMAL) {
-                        if (res.data.data_count == 0) {
-                            this.notify({title: 'AP連線數', message: '無資料，無法繪製圖形', type: 'warning'});
-                        } else {
-                            //this.items.length = 0;
-                            res.data.raw.reverse().forEach(item => {
-                                // e.g. item => { count: 911, ip: "220.1.35.123", log_time: "20200904175957", site: "HB" }
-                                if (item.site == 'TOTAL') { this.total_count = item.count; }
-                                else if (item.site == 'DB') { this.db_count = item.count; }
-                                else {
-                                    let found = this.items.find((oitem, index, array) => {
-                                        return item.site == oitem[0];
-                                    });
-                                    if (found) {
-                                        found[1] = item.count;
-                                    } else {
-                                        // chart item format is array => ['text', 'count']
-                                        this.items.push([item.site, item.count]);
+                if (this.isOfficeHours()) {
+                    this.isBusy = true;
+                    this.$http.post(CONFIG.API.JSON.STATS, {
+                        type: "stats_ap_conn_latest",
+                        ip: this.ip,
+                        count: 11
+                    }).then(res => {
+                        console.assert(res.data.status == XHR_STATUS_CODE.SUCCESS_NORMAL, `取得AP連線數回傳狀態碼有問題【${res.data.status}】`);
+                        if (res.data.status == XHR_STATUS_CODE.SUCCESS_NORMAL) {
+                            if (res.data.data_count == 0) {
+                                this.notify({title: 'AP連線數', message: '無資料，無法繪製圖形', type: 'warning'});
+                            } else {
+                                //this.items.length = 0;
+                                res.data.raw.reverse().forEach(item => {
+                                    // e.g. item => { count: 911, ip: "220.1.35.123", log_time: "20200904175957", site: "HB" }
+                                    if (item.site == 'TOTAL') { this.total_count = item.count; }
+                                    else if (item.site == 'DB') { this.db_count = item.count; }
+                                    else {
+                                        let found = this.items.find((oitem, index, array) => {
+                                            return item.site == oitem[0];
+                                        });
+                                        if (found) {
+                                            found[1] = item.count;
+                                        } else {
+                                            // chart item format is array => ['text', 'count']
+                                            this.items.push([item.site, item.count]);
+                                        }
                                     }
-                                }
-                            });
+                                });
+                            }
+                        } else {
+                            this.alert({title: `取得${this.ip}連線數`, message: `取得AP連線數回傳狀態碼有問題【${res.data.status}】`, variant: "warning"});
                         }
-                    } else {
-                        this.alert({title: `取得${this.ip}連線數`, message: `取得AP連線數回傳狀態碼有問題【${res.data.status}】`, variant: "warning"});
-                    }
-                }).catch(err => {
-                    this.error = err;
-                }).finally(() => {
-                    this.isBusy = false;
-                    // reload every 15s
-                    setTimeout(this.reload, 15000);
-                });
+                    }).catch(err => {
+                        this.error = err;
+                    }).finally(() => {
+                        this.isBusy = false;
+                        // reload every 15s
+                        setTimeout(this.reload, 15000);
+                    });
+                } else {
+                    // check after an hour
+                    setTimeout(this.reload, 3600000);
+                }
             }
         },
         created() {
