bb21ff53deb48e5372e6a935208c9e6d08fa935c
pyliu-home <3634168+pyliu@users.noreply.github.com>
2020-09-06 12:48:56 +0800

    use title

--- www/assets/js/components/dashboards/lah-xap-history-chart.js
+++ www/assets/js/components/dashboards/lah-xap-history-chart.js
@@ -2,7 +2,7 @@ if (Vue) {
     Vue.component('lah-xap-history-chart', {
         template: `<div>
             <div class="text-justify">
-                <span class="align-middle small">{{site}}</span>
+                <span class="align-middle small">{{title}}</span>
                 <b-button-group size="sm" class="float-right">
                     <b-button variant="primary" @click="type = 'bar'"><i class="fas fa-chart-bar"></i></b-button>
                     <b-button variant="secondary" @click="type = 'pie'"><i class="fas fa-chart-pie"></i></b-button>
@@ -33,10 +33,10 @@ if (Vue) {
                         type: "stats_ap_conn_HX_history",
                         site: this.site
                     }).then(res => {
-                        console.assert(res.data.status == XHR_STATUS_CODE.SUCCESS_NORMAL, `取得跨所 AP H0 歷史連線數回傳狀態碼有問題【${res.data.status}】`);
+                        console.assert(res.data.status == XHR_STATUS_CODE.SUCCESS_NORMAL, `取得跨所 AP ${this.site} 歷史連線數回傳狀態碼有問題【${res.data.status}】`);
                         if (res.data.status == XHR_STATUS_CODE.SUCCESS_NORMAL) {
                             if (res.data.data_count == 0) {
-                                this.notify({title: '跨所 AP H0 歷史連線數', message: '無資料，無法繪製圖形', type: 'warning'});
+                                this.notify({title: `跨所 AP ${this.site} 歷史連線數`, message: '無資料，無法繪製圖形', type: 'warning'});
                             } else {
                                 res.data.raw.forEach(item => {
                                     // e.g. item => { count: 34, ip: "220.1.35.123", log_time: "20200904175957", site: "H0" }
@@ -52,7 +52,7 @@ if (Vue) {
                                 });
                             }
                         } else {
-                            this.alert({title: `取得跨所 AP H0 歷史連線數`, message: `取得跨所 AP H0 歷史連線數回傳狀態碼有問題【${res.data.status}】`, variant: "warning"});
+                            this.alert({title: `取得跨所 AP ${this.site} 歷史連線數`, message: `取得跨所 AP ${this.site} 歷史連線數回傳狀態碼有問題【${res.data.status}】`, variant: "warning"});
                         }
                     }).catch(err => {
                         this.error = err;
@@ -71,7 +71,6 @@ if (Vue) {
             this.reload(true);
         }
     });
-        console.log(Vue.options.components);
 } else {
     console.error("vue.js not ready ... lah-xap-history-chart component can not be loaded.");
 }
