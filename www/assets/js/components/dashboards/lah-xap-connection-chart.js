c63f1a6f069d43e3228fcac930308ddef00f8468
pyliu <3634168+pyliu@users.noreply.github.com>
2020-09-11 10:22:34 +0800

    use alert window if db conn approaching blow up

--- www/assets/js/components/dashboards/lah-xap-connection-chart.js
+++ www/assets/js/components/dashboards/lah-xap-connection-chart.js
@@ -50,11 +50,19 @@ if (Vue) {
             },
             db_count(val) {
                 if (val > 2500) {
-                    this.notify({
-                        title: '跨所AP資料庫連線數過高警示',
-                        type: 'danger',
-                        message: `<i class="fas fa-exclamation-circle"></i> 目前占用資料庫連線數已達達 ${val}，須注意!`
-                    })
+                    if (val > 3000) {
+                        this.alert({
+                            title: '跨所AP資料庫連線數過高警示',
+                            type: 'danger',
+                            message: `<i class="fas fa-exclamation-circle"></i> 目前占用資料庫連線數已達 ${val}，請立即處理！` 
+                        });
+                    } else {
+                        this.notify({
+                            title: '跨所AP資料庫連線數過高通知',
+                            type: val > 3000 ? 'dark' : 'danger',
+                            message: `<i class="fas fa-exclamation-circle"></i> 目前占用資料庫連線數已達 ${val}，須注意!` 
+                        });
+                    }
                 }
             }
         },
