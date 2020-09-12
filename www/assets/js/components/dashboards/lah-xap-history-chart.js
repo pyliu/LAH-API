8ba589ebbd7f1bc1d2aa3d5db98605d5a941ecd0
pyliu <3634168+pyliu@users.noreply.github.com>
2020-09-12 11:53:12 +0800

    use tremble for the red condition

--- www/assets/js/components/dashboards/lah-xap-history-chart.js
+++ www/assets/js/components/dashboards/lah-xap-history-chart.js
@@ -58,7 +58,7 @@ if (Vue) {
         methods: {
             style_by_count(value, opacity = 0.6) {
                 let variant, action, rgb;
-                if (value > 200) { variant = 'danger'; action='coin-h'; rgb = `rgb(243, 0, 19, ${opacity})`}        // red
+                if (value > 200) { variant = 'danger'; action='tremble'; rgb = `rgb(243, 0, 19, ${opacity})`}        // red
                 else if (value > 100) { variant = 'warning'; action='beat'; rgb = `rgb(238, 182, 1, ${opacity})`;}  // yellow
                 else if (value > 5) { variant = 'success'; action='jump'; rgb = `rgb(0, 200, 0, ${opacity})`}      // green
                 else { variant = 'muted'; action= ''; rgb = `rgb(207, 207, 207, ${opacity})`; }                     // muted
