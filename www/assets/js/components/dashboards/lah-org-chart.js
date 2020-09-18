f85604cec20b7d9b60a0dade11991baf6752bdaa
pyliu <3634168+pyliu@users.noreply.github.com>
2020-09-18 15:48:26 +0800

    refactoring

--- www/assets/js/components/dashboards/lah-org-chart.js
+++ www/assets/js/components/dashboards/lah-org-chart.js
@@ -6,51 +6,84 @@ if (Vue) {
             reload_timer: null,
             resize_timer: null,
             config: null,
-            depth: 0
+            depth: 0,
+            margin: 15
         }),
         methods: {
             reload() {
-                this.isBusy = true;
-                clearTimeout(this.reload_timer);
-                this.$http.post(CONFIG.API.JSON.QUERY, {
-                    type: "org_data"
-                }).then(res => {
-                    console.assert(res.data.status == XHR_STATUS_CODE.SUCCESS_NORMAL, `取得組織樹狀資料回傳狀態碼有問題【${res.data.status}】`);
-                    if (res.data.status == XHR_STATUS_CODE.SUCCESS_NORMAL) {
-                        this.config = {
-                            chart: {
-                                container: `#e6e03333-5899-4cad-b934-83189668a148`,
-                                connectors: {
-                                    type: 'step' // curve, bCurve, step, straight
-                                },
-                                node: {
-                                    HTMLclass: 'mynode',
-                                    // collapsable: true,
-                                    stackChildren: true
-
-                                },
-                                rootOrientation: 'NORTH',
-                                nodeAlign: 'TOP',
-                                animateOnInit: false
-                            },
-                            nodeStructure: this.nodeStructure(res.data.raw)
-                        };
-                        this.reload_timer = this.delay(() => {
-                            this.inst = new Treant(this.config);
-                            this.depth = 0;
-                        }, 1000);
-                    } else {
-                        this.alert({
-                            title: `取得組織樹狀資料`,
-                            message: `取得組織樹狀資料回傳狀態碼有問題【${res.data.status}】`,
-                            variant: "warning"
+                this.getLocalCache('lah-org-chart').then(cached => {
+                    if (cached === false) {
+                        this.isBusy = true;
+                        this.$http.post(CONFIG.API.JSON.QUERY, {
+                            type: "org_data"
+                        }).then(res => {
+                            console.assert(res.data.status == XHR_STATUS_CODE.SUCCESS_NORMAL, `取得組織樹狀資料回傳狀態碼有問題【${res.data.status}】`);
+                            if (res.data.status == XHR_STATUS_CODE.SUCCESS_NORMAL) {
+                                this.setLocalCache('lah-org-chart', res.data.raw);
+                                this.build(res.data.raw);
+                            } else {
+                                this.alert({
+                                    title: `取得組織樹狀資料`,
+                                    message: `取得組織樹狀資料回傳狀態碼有問題【${res.data.status}】`,
+                                    variant: "warning"
+                                });
+                            }
+                        }).catch(err => {
+                            this.error = err;
+                        }).finally(() => {
+                            this.isBusy = false;
                         });
+                    } else {
+                        this.build(cached);
                     }
-                }).catch(err => {
-                    this.error = err;
-                }).finally(() => {
-                    this.isBusy = false;
                 });
+            },
+            build(raw) {
+                clearTimeout(this.reload_timer);
+                this.reload_timer = this.delay(() => {
+                    this.isBusy = true;
+                    this.prepareConfig(raw);
+                    this.inst = new Treant(this.config, () => {
+                        this.depth = 0;
+                        this.isBusy = false;
+                    }, $);
+                    // this.$log(this.inst);
+                }, 1000);
+            },
+            prepareConfig(raw) {
+                this.config = {
+                    chart: {
+                        container: `#e6e03333-5899-4cad-b934-83189668a148`,
+                        connectors: {
+                            type: 'step' // curve, bCurve, step, straight
+                        },
+                        node: {
+                            HTMLclass: 'mynode',
+                            // collapsable: true,
+                            // stackChildren: true
+                        },
+                        rootOrientation: 'WEST',
+                        // animateOnInit: false,
+                        // nodeAlign: 'TOP',
+                        siblingSeparation: this.margin,
+                        levelSeparation: this.margin,
+                        subTeeSeparation: this.margin/*,
+                        callback: {
+                            onCreateNode: null,
+                            onCreateNodeCollapseSwitch: null,
+                            onAfterAddNode: null,
+                            onBeforeAddNode: null,
+                            onAfterPositionNode: null,
+                            onBeforePositionNode: null,
+                            onToggleCollapseFinished: null,
+                            onAfterClickCollapseSwitch: null,
+                            onBeforeClickCollapseSwitch: null,
+                            onTreeLoaded: null
+                        }
+                        */
+                    },
+                    nodeStructure: this.nodeStructure(raw)
+                };
             },
             nodeStructure(raw_obj) {
                 this.depth++;
@@ -67,16 +100,18 @@ if (Vue) {
                     } );
                 }
                 this.depth--;
+                let collapsable = this.depth_switch && children.length > 0;
                 return {
                     text: {
                         name: `${raw_obj.id}:${raw_obj.name}`,
                         title: raw_obj.title,
-                        //contact: raw_obj.tel
+                        contact: `${raw_obj.ext}`,
+                        desc: raw_obj.work
                     },
                     image: `assets/img/users/${raw_obj.name}_avatar.jpg`,
-                    collapsable: this.depth_switch && children.length > 0,
-                    collapsed: this.depth_switch && children.length > 0,
-                    stackChildren: this.depth_switch,
+                    collapsable: collapsable,
+                    collapsed: collapsable && !(raw_obj.authority & 4 && raw_obj.unit == '資訊課'),
+                    stackChildren: this.depth_switch && children.length > 1,
                     HTMLclass: "mynode bg-muted",
                     pseudo: false,
                     children: [...children]
