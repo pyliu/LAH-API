3fff714e4546676b9b3477e5254607e0d0a0054d
pyliu <3634168+pyliu@users.noreply.github.com>
2020-09-22 09:41:28 +0800

    copy tmp.txt to another file

--- www/api/export_tmp_txt.php
+++ www/api/export_tmp_txt.php
@@ -3,6 +3,17 @@ require_once(dirname(dirname(__FILE__)).DIRECTORY_SEPARATOR."include".DIRECTORY_
 
 $tmp_file = ROOT_DIR.DIRECTORY_SEPARATOR.'exports'.DIRECTORY_SEPARATOR.'tmp.txt';
 
+if (isset($_SESSION['export_tmp_txt_filename'])) {
+    // copy tmp.txt to the target as well
+    $target = ROOT_DIR.DIRECTORY_SEPARATOR.'exports'.DIRECTORY_SEPARATOR.$_SESSION['export_tmp_txt_filename'].'.txt';
+    $res = @copy($tmp_file, $target);
+    if ($res) {
+        $log->info('Copied tmp.txt to '.$target);
+    } else {
+        $log->error('Cannot copy tmp.txt to '.$target);
+    }
+}
+
 header("Content-Length: " . filesize($tmp_file));
 // header('Content-Type: text/plain');
 header('Content-Disposition: attachment; filename='.($_GET['filename'] ?? 'tmp').'.txt');
