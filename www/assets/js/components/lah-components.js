5951f5cd1c0785a9a0e125ba78014b5a0841d328
pyliu <3634168+pyliu@users.noreply.github.com>
2020-09-23 21:56:43 +0800

    move temperature in the sub-menu

--- www/assets/js/components/lah-components.js
+++ www/assets/js/components/lah-components.js
@@ -359,11 +359,6 @@ if (Vue) {
                 need_admin: false,
                 children: []
             }, {
-                text: "體溫紀錄",
-                url: "temperature.html",
-                icon: "head-side-mask",
-                need_admin: false
-            }, {
                 text: `統計看板`,
                 url: "stats.html",
                 icon: "laptop-house",
@@ -379,6 +374,11 @@ if (Vue) {
                 icon: "hands-helping",
                 need_admin: false,
                 children: [{
+                    text: "體溫紀錄",
+                    url: "temperature.html",
+                    icon: "head-side-mask",
+                    need_admin: false
+                }, {
                     text: "信差訊息",
                     url: "message.html",
                     icon: "comments",
