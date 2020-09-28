f19c47e5948e484b039857a6434a9c65174881d1
pyliu <3634168+pyliu@users.noreply.github.com>
2020-09-28 23:46:04 +0800

    cache support expire

--- www/include/Cache.class.php
+++ www/include/Cache.class.php
@@ -14,14 +14,15 @@ class Cache {
 
     function __destruct() { }
     
-    public function set($key, $val) {
+    public function set($key, $val, $expire = 864000) {
         if ($this->system->isMockMode()) return false;
         $stm = $this->sqlite3->prepare("
-            REPLACE INTO cache ('key', 'value')
-            VALUES (:key, :value)
+            REPLACE INTO cache ('key', 'value', 'expire')
+            VALUES (:key, :value, :expire)
         ");
         $stm->bindParam(':key', $key);
         $stm->bindParam(':value', $val);
+        $stm->bindParam(':expire', $expire);
         return $stm->execute() === FALSE ? false : true;
     }
 
@@ -29,6 +30,13 @@ class Cache {
         $val = $this->sqlite3->querySingle("SELECT value from cache WHERE key = '$key'");
         if (empty($val)) return false;
         return unserialize($val);
+    }
+
+    
+    public function getExpire($key) {
+        $val = $this->sqlite3->querySingle("SELECT expire from cache WHERE key = '$key'");
+        if (empty($val)) return false;
+        return intval($val);
     }
 }
 ?>
