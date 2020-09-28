1ae5b8804f1da06644da0166ba07cd81c0c31593
pyliu <3634168+pyliu@users.noreply.github.com>
2020-09-28 23:28:11 +0800

    use cache sqlite db

--- www/include/Cache.class.php
+++ www/include/Cache.class.php
@@ -1,25 +1,34 @@
 <?php
 require_once("init.php");
 require_once(ROOT_DIR."/include/System.class.php");
-
-define("CACHE_ROOT_DIR", ROOT_DIR.DIRECTORY_SEPARATOR."assets".DIRECTORY_SEPARATOR."cache");
+define('CACHE_SQLITE_DB', ROOT_DIR.DIRECTORY_SEPARATOR."assets".DIRECTORY_SEPARATOR."db".DIRECTORY_SEPARATOR."cache.db");
 
 class Cache {
     private $system;
+    private $sqlite3;
 
-    function __construct() { $this->system = new System(); }
+    function __construct() {
+        $this->sqlite3 = new SQLite3(CACHE_SQLITE_DB);
+        $this->system = new System();
+    }
 
     function __destruct() { }
     
     public function set($key, $val) {
-        if (!$this->system->isMockMode()) {
-            return file_put_contents(CACHE_ROOT_DIR.DIRECTORY_SEPARATOR.$key.".cache", serialize($val));
-        }
-        return false;
+        if ($this->system->isMockMode()) return false;
+        $stm = $this->sqlite3->prepare("
+            REPLACE INTO cache ('key', 'value')
+            VALUES (:key, :value)
+        ");
+        $stm->bindParam(':key', $key);
+        $stm->bindParam(':value', $val);
+        return $stm->execute() === FALSE ? false : true;
     }
 
     public function get($key) {
-        return unserialize(file_get_contents(CACHE_ROOT_DIR.DIRECTORY_SEPARATOR.$key.".cache"));
+        $val = $this->sqlite3->querySingle("SELECT value from cache WHERE key = '$key'");
+        if (empty($val)) return false;
+        return unserialize($val);
     }
 }
 ?>
