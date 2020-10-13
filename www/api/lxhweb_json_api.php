3fe4786f6ef0596161414154fed04dd4a993548f
pyliu <3634168+pyliu@users.noreply.github.com>
2020-10-13 14:19:32 +0800

    reduce log

--- www/api/lxhweb_json_api.php
+++ www/api/lxhweb_json_api.php
@@ -12,7 +12,7 @@ $mock = $system->isMockMode();
 
 switch ($_POST["type"]) {
 	case "l3hweb_site_update_time":
-        $log->info("XHR [l3hweb_site_update_time] 查詢同步異動更新時間 請求 ".$_POST["site"]);
+        // $log->info("XHR [l3hweb_site_update_time] 查詢各所同步異動更新時間 請求 ".$_POST["site"]);
 		$rows = $mock ? $cache->get('l3hweb_site_update_time') : $l3hweb->querySiteUpdateTime();
 		if (!$mock) $cache->set('l3hweb_site_update_time', $rows);
 		$count = $rows === false ? 0 : count($rows);
