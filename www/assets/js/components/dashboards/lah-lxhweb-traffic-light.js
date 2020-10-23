c37bf72bf3c68a929066c94462d10c2dbdf764a2
pyliu <3634168+pyliu@users.noreply.github.com>
2020-10-23 18:06:20 +0800

    traffic light to 'lg'

--- www/assets/js/components/dashboards/lah-lxhweb-traffic-light.js
+++ www/assets/js/components/dashboards/lah-lxhweb-traffic-light.js
@@ -17,7 +17,7 @@ if (Vue) {
         <div v-if="alive" class="h-100">
           <div v-if="type == 'light'" :id="container_id" class="grids">
             <div v-for="entry in list" class="grid-4col-2row">
-              <lah-fa-icon icon="circle" :variant="light(entry)" :action="action(entry)" v-b-popover.hover.focus.top="'最後更新時間: '+entry.UPDATE_DATETIME">{{name(entry)}}</lah-fa-icon>
+              <lah-fa-icon size="lg" icon="circle" :variant="light(entry)" :action="action(entry)" v-b-popover.hover.focus.top="'最後更新時間: '+entry.UPDATE_DATETIME">{{name(entry)}}</lah-fa-icon>
             </div>
           </div>
           <div v-else :id="container_id">
