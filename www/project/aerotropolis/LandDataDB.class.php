96637bfaa0c8cc605577f9fe54f500133c4d7753
pyliu <3634168+pyliu@users.noreply.github.com>
2020-12-08 18:31:32 +0800

    admin feature ready

--- www/project/aerotropolis/LandDataDB.class.php
+++ www/project/aerotropolis/LandDataDB.class.php
@@ -32,17 +32,16 @@ class LandDataDB {
         ');
         $sqlite->createTableBySQL('
             CREATE TABLE IF NOT EXISTS "people_data_mapping" (
-                "id"	INTEGER,
                 "houshold"	TEXT NOT NULL,
                 "pid"	TEXT NOT NULL,
                 "pname"	TEXT NOT NULL,
                 "owned_number"	TEXT,
-                PRIMARY KEY("id" AUTOINCREMENT)
+                PRIMARY KEY("pid","houshold")
             )
         ');
         $sqlite->createTableBySQL('
-            CREATE INDEX IF NOT EXISTS "pid_owned" ON "people_data_mapping" (
-                "pid",
+            CREATE INDEX IF NOT EXISTS "pname_owned_number" ON "people_data_mapping" (
+                "pname",
                 "owned_number"
             )
         ');
@@ -52,9 +51,15 @@ class LandDataDB {
     function __construct() {
         $path = $this->getLandDataDB();
         $this->db = new SQLite3($path);
+        $this->db->exec("PRAGMA cache_size = 100000");
+        $this->db->exec("PRAGMA temp_store = MEMORY");
+        $this->db->exec("BEGIN TRANSACTION");
     }
 
-    function __destruct() { $this->db->close(); }
+    function __destruct() {
+        $this->db->exec("END TRANSACTION");
+        $this->db->close();
+    }
 
     public function addLandData($code, $name, $number, $content) {
         $stm = $this->db->prepare("INSERT INTO data_mapping ('code', 'name', 'number', 'content') VALUES (:code, :name, :number, :content)");
@@ -70,118 +75,13 @@ class LandDataDB {
         $stm->bindParam(':code', $code);
         return $stm->execute() === FALSE ? false : true;
     }
-    /**
-     * Early LAH Stats
-     */
-    /*
-    public function getTotal($id) {
-        return $this->db->querySingle("SELECT TOTAL from stats WHERE ID = '$id'");
-    }
 
-    public function updateTotal($id, $total) {
-        $stm = $this->db->prepare("UPDATE stats set TOTAL = :total WHERE  ID = :id");
-        $stm->bindValue(':total', intval($total));
-        $stm->bindParam(':id', $id);
+    public function addPeopleMapping($household, $pid, $pname, $owned_number) {
+        $stm = $this->db->prepare("REPLACE INTO people_data_mapping ('houshold', 'pid', 'pname', 'owned_number') VALUES (:household, :pid, :pname, :owned_number)");
+        $stm->bindParam(':household', $household);
+        $stm->bindParam(':pid', $pid);
+        $stm->bindParam(':pname', $pname);
+        $stm->bindParam(':owned_number', $owned_number);
         return $stm->execute() === FALSE ? false : true;
     }
-
-    public function addOverdueMsgCount($count = 1) {
-        global $log;
-        $total = $this->getTotal('overdue_msg_count') + $count;
-        $ret = $this->updateTotal('overdue_msg_count', $total);
-        $log->info(__METHOD__.": overdue_msg_count 計數器+${count}，目前值為 ${total} 【".($ret ? "成功" : "失敗")."】");
-    }
-
-    public function addOverdueStatsDetail($data) {
-        // $data => ["ID" => HB0000, "RECORDS" => array, "DATETIME" => 2020-03-04 08:50:23, "NOTE" => XXX]
-        // overdue_stats_detail
-        $stm = $this->db->prepare("INSERT INTO overdue_stats_detail (datetime,id,count,note) VALUES (:date, :id, :count, :note)");
-        $stm->bindParam(':date', $data["DATETIME"]);
-        $stm->bindParam(':id', $data["ID"]);
-        $stm->bindValue(':count', count($data["RECORDS"]));
-        $stm->bindParam(':note', $data["NOTE"]);
-        $ret = $stm->execute();
-        if (!$ret) {
-            global $log;
-            $log->error(__METHOD__.": 新增逾期統計詳情失敗【".$stm->getSQL()."】");
-        }
-    }
-
-    public function addXcasesStats($data) {
-        // $data => ["date" => "2020-03-04 10:10:10","found" => 2, "note" => XXXXXXXXX]
-        // xcase_stats
-        $stm = $this->db->prepare("INSERT INTO xcase_stats (datetime,found,note) VALUES (:date, :found, :note)");
-        $stm->bindParam(':date', $data["date"]);
-        $stm->bindParam(':found', $data["found"]);
-        $stm->bindParam(':note', $data["note"]);
-        $ret = $stm->execute();
-        global $log;
-        $log->info(__METHOD__.": 新增跨所註記遺失案件統計".($ret ? "成功" : "失敗【".$stm->getSQL()."】")."。");
-        // 更新 total counter
-        $total = $this->getTotal('xcase_found_count') + $data["found"];
-        $ret = $this->updateTotal('xcase_found_count', $total);
-        $log->info(__METHOD__.": xcase_found_count 計數器+".$data["found"]."，目前值為 ${total} 【".($ret ? "成功" : "失敗")."】");
-
-    }
-
-    public function addBadSurCaseStats($data) {
-        // $data => ["date" => "2020-03-04 10:10:10","found" => 2, "note" => XXXXXXXXX]
-        // xcase_stats
-        $stm = $this->db->prepare("INSERT INTO found_bad_sur_case_stats (datetime,found,note) VALUES (:date, :found, :note)");
-        $stm->bindParam(':date', $data["date"]);
-        $stm->bindParam(':found', $data["found"]);
-        $stm->bindParam(':note', $data["note"]);
-        $ret = $stm->execute();
-        global $log;
-        $log->info(__METHOD__.": 新增複丈問題案件統計".($ret ? "成功" : "失敗【".$stm->getSQL()."】")."。");
-        // 更新 total counter
-        $total = $this->getTotal('bad_sur_case_found_count') + $data["found"];
-        $ret = $this->updateTotal('bad_sur_case_found_count', $total);
-        $log->info(__METHOD__.": bad_sur_case_found_count 計數器+".$data["found"]."，目前值為 ${total} 【".($ret ? "成功" : "失敗")."】");
-
-    }
-
-    public function addStatsRawData($id, $data) {
-        // $data => php array
-        // overdue_stats_detail
-        $stm = $this->db->prepare("INSERT INTO stats_raw_data (id,data) VALUES (:id, :data)");
-        $param = serialize($data);
-        $stm->bindParam(':data', $param);
-        $stm->bindParam(':id', $id);
-        $ret = $stm->execute();
-        if (!$ret) {
-            global $log;
-            $log->error(__METHOD__.": 新增統計 RAW DATA 失敗【".$id.", ".$stm->getSQL()."】");
-        }
-        return $ret;
-    }
-
-    public function removeAllStatsRawData($year_month) {
-        $stm = $this->db->prepare("DELETE FROM stats_raw_data WHERE id LIKE '%_".$year_month."'");
-        $ret = $stm->execute();
-        if (!$ret) {
-            global $log;
-            $log->error(__METHOD__.": 移除統計 RAW DATA 失敗【".$year_month.", ".$stm->getSQL()."】");
-        }
-        return $ret;
-    }
-
-    public function removeStatsRawData($id) {
-        // $data => php array
-        // overdue_stats_detail
-        $stm = $this->db->prepare("DELETE FROM stats_raw_data WHERE id = :id");
-        $stm->bindParam(':id', $id);
-        $ret = $stm->execute();
-        if (!$ret) {
-            global $log;
-            $log->error(__METHOD__.": 移除統計 RAW DATA 失敗【".$id.", ".$stm->getSQL()."】");
-        }
-        return $ret;
-    }
-
-    public function getStatsRawData($id) {
-        $data = $this->db->querySingle("SELECT data from stats_raw_data WHERE id = '$id'");
-        return empty($data) ? false : unserialize($data);
-    }
-    */
 }
--- www/project/aerotropolis/import_land_txt.php
+++ www/project/aerotropolis/import_land_txt.php
@@ -2,8 +2,6 @@
 require_once("init.php");
 require_once(ROOT_DIR.DIRECTORY_SEPARATOR."LandDataDB.class.php");
 
-// $log->info(print_r($_FILES, true));
-
 // Function to check the string is ends 
 // with given substring or not 
 function reachEnd($string, $endString) { 
@@ -26,9 +24,13 @@ if (isset($_FILES['file']['name'])) {
         $path = $_FILES['file']['tmp_name'];
         if (is_file($path)) {
             $message = '上傳成功';
-
-            // remove old file first
-            $exist_files = glob(FILE_PATH.DIRECTORY_SEPARATOR.$found_section_code."*.txt");
+            // savew txt to sub section code folder
+            $saved_dir = FILE_PATH.DIRECTORY_SEPARATOR.$found_section_code;
+            if (!is_dir($saved_dir)) {
+                mkdir($saved_dir);
+            }
+            // remove old files first
+            $exist_files = glob($saved_dir.DIRECTORY_SEPARATOR."*.txt");
             array_map('unlink', $exist_files);
 
             $tmp_file = new SplFileObject($path);
@@ -43,7 +45,7 @@ if (isset($_FILES['file']['name'])) {
                 if ($now_found) {
                     $now_content .= $line;
                     if (reachEnd($now_content, "\n\n\n")) {
-                        $saved_path = FILE_PATH.DIRECTORY_SEPARATOR.$found_section_code.str_replace('-', '', $now_land_number).".txt";
+                        $saved_path = $saved_dir.DIRECTORY_SEPARATOR.str_replace('-', '', $now_land_number).".txt";
                         $prev_content = @file_get_contents($saved_path);
                         $current_content = mb_convert_encoding($now_content, 'UTF-8', 'BIG5');
                         if ($prev_content === false) {
