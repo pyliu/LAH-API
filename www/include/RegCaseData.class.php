ce60b78d037e46ab89fe687ba24f5a066d1dc007
pyliu <3634168+pyliu@users.noreply.github.com>
2020-12-22 18:36:09 +0800

    wording

--- www/include/RegCaseData.class.php
+++ www/include/RegCaseData.class.php
@@ -274,7 +274,7 @@ class RegCaseData {
             case '12':
                 return '觀音區';
             default:
-                return (array_key_exists($this->row["RM100"], OFFICE) ? OFFICE[$this->row["RM100"]] : $this->row["RM100"]).'所轄區';
+                return (array_key_exists($this->row["RM100"], OFFICE) ? OFFICE[$this->row["RM100"]].'所' : $this->row["RM100"]).'轄區';
         }
     }
 
