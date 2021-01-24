a9f0a1dc2e9bec39573545c67c091bab611369ca
pyliu <3634168+pyliu@users.noreply.github.com>
2021-01-24 21:24:33 +0800

    also copy name copy jpg

--- www/api/upload_user_photo.php
+++ www/api/upload_user_photo.php
@@ -15,14 +15,24 @@ if (isset($_FILES['file']['name']) && isset($_FILES['file']['tmp_name'])) {
         $user_id = $_POST['id'];
         $user_name = $_POST['name'];
         $avatar = $_POST['avatar'] === "true";
+
+        // use ID as the file name
         $to_filename = "${user_id}".($avatar ? "_avatar" : "").".${extension}";
         $to_file = USER_IMG_DIR.DIRECTORY_SEPARATOR.$to_filename;
-        // prerequisite is ready
         if(copy($tmp_file, $to_file)){
             $status = STATUS_CODE::SUCCESS_NORMAL;
             $message = '已儲存 '.$filename.' => '.$to_filename;
         } else {
             $message = '複製失敗 '.$tmp_file.' => '.$to_file;
+        }
+
+        // also use name as the file name
+        $to_filename = "${user_name}".($avatar ? "_avatar" : "").".${extension}";
+        $to_file = USER_IMG_DIR.DIRECTORY_SEPARATOR.$to_filename;
+        if(copy($tmp_file, $to_file)){
+            $message .= '\n已儲存 '.$filename.' => '.$to_filename;
+        } else {
+            $message .= '\n複製失敗 '.$tmp_file.' => '.$to_file;
         }
     } else {
         $message = "檔案不是JPG";
