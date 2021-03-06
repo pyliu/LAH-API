9c460d57920d39d49ce8936d5c881d7286b800cf
pyliu <3634168+pyliu@users.noreply.github.com>
2021-03-06 15:59:33 +0800

    add exclude_type_list for query json api

--- www/api/query_json_api.php
+++ www/api/query_json_api.php
@@ -21,14 +21,18 @@ $system = System::getInstance();
 
 $mock = $system->isMockMode();
 
-$db_ip = $system->getOraTargetDBIP();
-$db_port = $system->getOraTargetDBPort();
-$latency = $system->ping($db_ip, $db_port, 1);
-if ($latency > 999 || $latency == '') {
-	$msg = "API伺服器無法連線至DB，請確認網路可連線至 $db_ip:$db_port!";
-	$log->error($msg);
-	echoJSONResponse($msg, STATUS_CODE::FAIL_REMOTE_UNREACHABLE);
-	exit;
+$exclude_type_list = array('configs', 'update_configs', 'ping', 'stats_overdue_msg_total', 'ip', 'svr', 'zip_log', 'remove_temperature', 'add_temperature', 'temperatures_by_id_date', 'temperatures_by_date', 'temperatures', 'xlsx_params');
+
+if (!in_array($_POST['type'], $exclude_type_list)) {
+	$db_ip = $system->getOraTargetDBIP();
+	$db_port = $system->getOraTargetDBPort();
+	$latency = $system->ping($db_ip, $db_port, 1);
+	if ($latency > 999 || $latency == '') {
+		$msg = "API伺服器無法連線至DB，請確認網路可連線至 $db_ip:$db_port!";
+		$log->error($msg);
+		echoJSONResponse($msg, STATUS_CODE::FAIL_REMOTE_UNREACHABLE);
+		exit;
+	}
 }
 
 switch ($_POST["type"]) {
