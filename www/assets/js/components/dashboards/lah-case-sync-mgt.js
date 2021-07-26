da87b2695c5b079c043a13b3e6aaeb56c07eff55
pyliu <3634168+pyliu@users.noreply.github.com>
2021-07-26 14:34:55 +0800

    no flash for case id exclamation-circle icon

--- www/assets/js/components/dashboards/lah-case-sync-mgt.js
+++ www/assets/js/components/dashboards/lah-case-sync-mgt.js
@@ -7,7 +7,7 @@ if (Vue) {
                         <lah-fa-icon icon="sync-alt">
                             同步登記案件
                             <span v-if="validate">{{ID}}</span>
-                            <lah-fa-icon v-else-if="!empty(ID)" icon="exclamation-circle" variant="danger" append action="damage" title="案件ID有問題">{{ID}}</lah-fa-icon>
+                            <lah-fa-icon v-else-if="!empty(ID)" icon="exclamation-circle" variant="danger" append title="案件ID有問題">{{ID}}</lah-fa-icon>
                         </lah-fa-icon>
                     </h6>
                     <b-button-group>
