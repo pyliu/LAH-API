37b30436df80111f541d20fd44df3b935b6dd616
pyliu <3634168+pyliu@users.noreply.github.com>
2021-08-20 16:51:11 +0800

    update regex to keep only digit and comma

--- www/include/System.class.php
+++ www/include/System.class.php
@@ -562,7 +562,7 @@ class System {
 
     public function getWebAPPostfix() {
         $str = $this->get('WEBAP_POSTFIXES') ?? '205,206,207,62,156,118,161,60';
-        return explode(',', preg_replace("/\s+/", '', $str));
+        return explode(',', preg_replace("/(\s+|[^0-9,])/", '', $str));
     }
 
     public function getSiteCode() {
