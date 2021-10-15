c229ef37e223ae58a4a79b77a609f6f6f9257699
pyliu <3634168+pyliu@users.noreply.github.com>
2021-10-15 09:02:23 +0800

    remove scripts

--- www/assets/scrips/7z壓縮DB.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-del C:\AppServ\www\assets\db\db.7z /F
-"c:\Program Files\7-Zip\7z" a C:\AppServ\www\assets\db\db.7z -p192168242 C:\AppServ\www\assets\db\*.db
--- www/assets/scrips/啟動WATCHDOG頁面.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-C:\AppServ\GoogleChromePortable64\GoogleChromePortable.exe http://localhost/tasklog.html -incognito
-
