367c94141c2e1bdeebdfbec3af3d12a190cc31aa
pyliu <3634168+pyliu@users.noreply.github.com>
2021-11-12 16:08:43 +0800

    no time limit to search problem xcase

--- www/include/Query.class.php
+++ www/include/Query.class.php
@@ -266,9 +266,9 @@ class Query {
 			return array();
 		}
 
-		global $week_ago;
-		$this->db->parse("SELECT * FROM SCRSMS WHERE RM07_1 >= :bv_week_ago AND RM02 LIKE 'H%".$this->site_code."1' AND (RM99 is NULL OR RM100 is NULL OR RM100_1 is NULL OR RM101 is NULL OR RM101_1 is NULL)");
-		$this->db->bind(":bv_week_ago", $week_ago);
+		// global $week_ago;
+		$this->db->parse("SELECT * FROM SCRSMS WHERE RM02 LIKE 'H%".$this->site_code."1' AND (RM99 is NULL OR RM100 is NULL OR RM100_1 is NULL OR RM101 is NULL OR RM101_1 is NULL)");
+		// $this->db->bind(":bv_week_ago", $week_ago);
         $this->db->execute();
         return $this->db->fetchAll();
 	}
