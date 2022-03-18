d0a8b5c2c0411c6c03c24d57ffca7977b3cee9a7
pyliu <3634168+pyliu@users.noreply.github.com>
2022-03-18 10:58:05 +0800

    default authority to 1 (需要辦畢通知)

--- www/include/SQLiteDBFactory.class.php
+++ www/include/SQLiteDBFactory.class.php
@@ -156,7 +156,7 @@ class SQLiteDBFactory {
         $sqlite->createTableBySQL('
             CREATE TABLE IF NOT EXISTS "reg_auth_checks_store" (
                 "case_no"	TEXT,
-                "authority"	INTEGER NOT NULL DEFAULT 0,
+                "authority"	INTEGER NOT NULL DEFAULT 1,
                 "note"	TEXT,
                 PRIMARY KEY("case_no")
             )
