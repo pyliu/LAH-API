302c448fa45398c6fcc85aa4e9f9a1f3a49965ae
pyliu <3634168+pyliu@users.noreply.github.com>
2022-04-25 10:52:41 +0800

    rare word skipping

--- www/include/RegCaseData.class.php
+++ www/include/RegCaseData.class.php
@@ -11,6 +11,9 @@ class RegCaseData {
     private $row;
 
     private function getIDorName($id) {
+        if (preg_match(self::$rare_word_regex, RegCaseData::$operators[$id])) {
+            return $id."(有罕字請參閱地政系統)";
+        }
         return preg_replace(self::$rare_word_regex, "○", RegCaseData::$operators[$id] ?? $id);
     }
 
@@ -206,16 +209,16 @@ class RegCaseData {
             "作業人員" => $this->getCurrentOperator(),
             "辦理情形" => $this->getStatus(),
             "權利人統編" => empty($row["RM18"]) ? "" : $row["RM18"],
-            "權利人姓名" => empty($row["RM19"]) ? "" : preg_replace(self::$rare_word_regex, "○", $row["RM19"]),
-            "權利人住址" => empty($row["RM18_ADDR"]) ? "" : preg_replace(self::$rare_word_regex, "○", $row["RM18_ADDR"]),
+            "權利人姓名" => empty($row["RM19"]) ? "" : (preg_match(self::$rare_word_regex, $row["RM19"]) ? '(包含罕字請參閱地政系統)' : preg_replace(self::$rare_word_regex, "○", $row["RM19"])),
+            "權利人住址" => empty($row["RM18_ADDR"]) ? "" : (preg_match(self::$rare_word_regex, $row["RM18_ADDR"]) ? '(包含罕字請參閱地政系統)' : preg_replace(self::$rare_word_regex, "○", $row["RM18_ADDR"])),
             "義務人統編" => empty($row["RM21"]) ? "" : $row["RM21"],
-            "義務人姓名" => empty($row["RM22"]) ? "" : preg_replace(self::$rare_word_regex, "○", $row["RM22"]),
-            "義務人住址" => empty($row["RM21_ADDR"]) ? "" : preg_replace(self::$rare_word_regex, "○", $row["RM21_ADDR"]),
+            "義務人姓名" => empty($row["RM22"]) ? "" : (preg_match(self::$rare_word_regex, $row["RM22"]) ? '(包含罕字請參閱地政系統)' : preg_replace(self::$rare_word_regex, "○", $row["RM22"])),
+            "義務人住址" => empty($row["RM21_ADDR"]) ? "" : (preg_match(self::$rare_word_regex, $row["RM21_ADDR"]) ? '(包含罕字請參閱地政系統)' : preg_replace(self::$rare_word_regex, "○", $row["RM21_ADDR"])),
             "義務人人數" => empty($row["RM23"]) ? "" : $row["RM23"],
             "手機號碼" => empty($row["RM102"]) ? "" : $row["RM102"],
             "代理人統編" => empty($row["RM24"]) ? "" : $row["RM24"],
-            "代理人姓名" => empty($row["AB02"]) ? "" : preg_replace(self::$rare_word_regex, "○", $row["AB02"]),
-            "代理人住址" => empty($row["AB03"]) ? "" : preg_replace(self::$rare_word_regex, "○", $row["AB03"]),
+            "代理人姓名" => empty($row["AB02"]) ? "" : (preg_match(self::$rare_word_regex, $row["AB02"]) ? '(包含罕字請參閱地政系統)' : preg_replace(self::$rare_word_regex, "○", $row["AB02"])),
+            "代理人住址" => empty($row["AB03"]) ? "" : (preg_match(self::$rare_word_regex, $row["AB03"]) ? '(包含罕字請參閱地政系統)' : preg_replace(self::$rare_word_regex, "○", $row["AB03"])),
             "代理人電話" => empty($row["AB04_1"]) ? "" : $row["AB04_1"].$row["AB04_2"],
             "非專代電話" => $row["AB04_NON_SCRIVENER_TEL"] ?? "",
             "區代碼" =>  $row["RM10"],
