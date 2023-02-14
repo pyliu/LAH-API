3520989e0902cd6976f02cdcf65565a80649824b
pyliu <3634168+pyliu@users.noreply.github.com>
2023-02-14 16:21:25 +0800

    add reg_json_api.php

---
+++ www/api/reg_json_api.php
@@ -0,0 +1,31 @@
+<?php
+require_once(dirname(dirname(__FILE__))."/include/init.php");
+require_once(INC_DIR."/RegCaseData.class.php");
+require_once(INC_DIR."/Query.class.php");
+require_once(INC_DIR."/Cache.class.php");
+require_once(INC_DIR."/System.class.php");
+
+$query = new Query();
+$cache = Cache::getInstance();
+$system = System::getInstance();
+$mock = $system->isMockMode();
+
+switch ($_POST["type"]) {
+    case "foreigner_pdf_list":
+        Logger::getInstance()->info("XHR [foreigner_pdf_list] get pdf list request.");
+        $count = 0;
+				// $query_result = $mock ? $cache->get('foreigner_pdf_list') : $query->getProblematicCrossCases();
+        // $cache->set('foreigner_pdf_list', $query_result);
+				// $count = count($query_result);
+				// $response_code = STATUS_CODE::SUCCESS_NORMAL;
+        $message = "取的 $count 筆PDF資料";
+        echoJSONResponse($message, $response_code, array(
+            "data" => $query_result,
+            "data_count" => $count
+				));
+        break;
+	default:
+		Logger::getInstance()->error("不支援的查詢型態【".$_POST["type"]."】");
+		echoJSONResponse("不支援的查詢型態【".$_POST["type"]."】", STATUS_CODE::UNSUPPORT_FAIL);
+		break;
+}
