ff637fef6928028bedf576e266dbd3ae77b5d55a
pyliu <3634168+pyliu@users.noreply.github.com>
2023-02-24 16:23:04 +0800

    reduce ident spaces

--- www/api/query_json_api.php
+++ www/api/query_json_api.php
@@ -20,27 +20,27 @@ $system = System::getInstance();
 $mock = $system->isMockMode();
 
 switch ($_POST["type"]) {
-    case "ping":
-        Logger::getInstance()->info("XHR [ping] Ping ".$_POST["ip"]." request.");
-        $ip = $_POST["ip"];
-        $ping = new Ping($ip, 1, 255);	// ip, timeout, ttl
-        $latency = 0;
-        if ($_POST['port']) {
-            $ping->setPort($_POST['port']);
-            $latency = $ping->ping('fsockopen');
-        } else {
-            $latency = $ping->ping();
-        }
-        $response_code = ($latency > 999 || $latency == '') ? STATUS_CODE::FAIL_TIMEOUT : STATUS_CODE::SUCCESS_NORMAL;
-        $message = "$ip 回應時間".(($latency > 999 || $latency == '') ? "逾時" : "為 $latency ms");
-        echo json_encode(array(
-            "status" => $response_code,
-            "ip" => $ip,
-            "latency" => empty($latency) ? "0" : $latency,
-            "data_count" => "1",
-            "message" => $message
-        ), 0);
-        break;
+	case "ping":
+		Logger::getInstance()->info("XHR [ping] Ping ".$_POST["ip"]." request.");
+		$ip = $_POST["ip"];
+		$ping = new Ping($ip, 1, 255);	// ip, timeout, ttl
+		$latency = 0;
+		if ($_POST['port']) {
+			$ping->setPort($_POST['port']);
+			$latency = $ping->ping('fsockopen');
+		} else {
+			$latency = $ping->ping();
+		}
+		$response_code = ($latency > 999 || $latency == '') ? STATUS_CODE::FAIL_TIMEOUT : STATUS_CODE::SUCCESS_NORMAL;
+		$message = "$ip 回應時間".(($latency > 999 || $latency == '') ? "逾時" : "為 $latency ms");
+		echo json_encode(array(
+			"status" => $response_code,
+			"ip" => $ip,
+			"latency" => empty($latency) ? "0" : $latency,
+			"data_count" => "1",
+			"message" => $message
+		), 0);
+		break;
 	case "stats_overdue_msg_total":
 		$stats = new StatsSQLite();
 		$total = $stats->getTotal('overdue_msg_count');
