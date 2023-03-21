a11c8b88ec49d86ca5c1fdcdbc5ef6ac9f290023
pyliu <3634168+pyliu@users.noreply.github.com>
2023-03-21 17:23:15 +0800

    remove useless code

--- www/include/RegQuery.class.php
+++ www/include/RegQuery.class.php
@@ -1,23 +1,12 @@
 <?php
 require_once("init.php");
-require_once("OraDB.class.php");
 require_once("System.class.php");
 require_once("RegCaseData.class.php");
 require_once("SQLiteRegForeignerPDF.class.php");
 
 class RegQuery {
-	private $site = 'HA';
-	private $site_code = 'A';
-	private $site_number = 1;
 
-	function __construct() {
-		$this->site = strtoupper(System::getInstance()->get('SITE')) ?? 'HA';
-		if (!empty($this->site)) {
-			$this->site_code = $this->site[1];
-			$this->site_number = ord($this->site_code) - ord('A');
-		}
-	}
-
+	function __construct() { }
 	function __destruct() {}
 
 	public function getRegForeignerPDF($st, $ed, $keyword = '') {
