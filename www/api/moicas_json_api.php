1c452ef8cc220f1785593d38ca4656621d98d001
pyliu <3634168+pyliu@users.noreply.github.com>
2023-06-13 11:01:54 +0800

    add crsms_records_by_clock type

--- www/api/moicas_json_api.php
+++ www/api/moicas_json_api.php
@@ -3,6 +3,8 @@ require_once(dirname(dirname(__FILE__)).DIRECTORY_SEPARATOR."include".DIRECTORY_
 require_once(INC_DIR.DIRECTORY_SEPARATOR."Cache.class.php");
 require_once(INC_DIR.DIRECTORY_SEPARATOR."System.class.php");
 require_once(INC_DIR.DIRECTORY_SEPARATOR."MOICAS.class.php");
+require_once(INC_DIR.DIRECTORY_SEPARATOR."MOICAS.class.php");
+require_once(INC_DIR.DIRECTORY_SEPARATOR."RegCaseData.class.php");
 
 $cache = Cache::getInstance();
 $system = System::getInstance();
@@ -10,6 +12,25 @@ $moicas = new MOICAS();
 $mock = $system->isMockMode();
 
 switch ($_POST["type"]) {
+	case "crsms_records_by_clock":
+		Logger::getInstance()->info("XHR [crsms_records_by_clock] check CMCRD empty temp record request.");
+		$rows = $mock ? $cache->get('crsms_records_by_clock') : $moicas->getCRSMSRecordsByClock($_POST['st'], $_POST['ed'], $_POST['clock']);
+		$cache->set('crsms_records_by_clock', $rows);
+		$message = is_array($rows) ? "目前查到 CRSMS 裡有 ".count($rows)." 筆資料" : '查詢 CRSMS 失敗';
+		$status_code = is_array($rows) ? STATUS_CODE::SUCCESS_NORMAL : STATUS_CODE::FAIL_DB_ERROR;
+		Logger::getInstance()->info("XHR [crsms_records_by_clock] $message");
+
+		$baked = array();
+		foreach ($rows as $row) {
+			$data = new RegCaseData($row);
+			$baked[] = $data->getBakedData();
+		}
+
+		echoJSONResponse($message, $status_code, array(
+			"raw" => $rows,
+			"baked" => $baked
+		));
+		break;
 	case "cmcrd_tmp_check":
 		Logger::getInstance()->info("XHR [cmcrd_tmp_check] check CMCRD empty temp record request.");
 		$rows = $mock ? $cache->get('cmcrd_tmp_check') : $moicas->getCMCRDTempRecords($_POST['year']);
--- www/include/MOICAS.class.php
+++ www/include/MOICAS.class.php
@@ -55,4 +55,44 @@ class MOICAS
 		$this->db_wrapper->getDB()->bind(":bv_mc02", $mc02);
 		return $this->db_wrapper->getDB()->execute() === FALSE ? false : true;
 	}
+	/**
+	 * get CRSMS records by clock 
+	 */
+	public function getCRSMSRecordsByClock($st, $ed, $clock)
+	{
+		global $today;
+		if (!$this->db_wrapper->reachable()) {
+			return array();
+		}
+		if (empty($st)) {
+			// default query is today
+			$st = $today;
+		}
+		if (empty($ed)) {
+			// default query is today
+			$ed = $today;
+		}
+		$val = intval($clock);
+		if ($val < 1 || $val > 23) {
+			Logger::getInstance()->warning(__METHOD__.": $clock 非合理小時區間，無法查詢。");
+			return array();
+		}
+		$this->db_wrapper->getDB()->parse("
+			SELECT
+				t.*,
+				s.KCNT AS \"RM09_CHT\"
+			FROM MOICAS.CRSMS t
+			LEFT JOIN MOIADM.RKEYN s ON s.KCDE_1 = '06' AND s.KCDE_2 = t.RM09
+			WHERE 1=1
+				AND (t.RM07_1 BETWEEN :bv_st And :bv_ed)
+				AND (t.RM101 = 'HA' OR t.RM101 IS NULL)
+				AND t.RM07_2 LIKE :bv_clock || '%'
+		");
+		$this->db_wrapper->getDB()->bind(":bv_st", $st);
+		$this->db_wrapper->getDB()->bind(":bv_ed", $ed);
+		$this->db_wrapper->getDB()->bind(":bv_clock", $clock);
+		$this->db_wrapper->getDB()->execute();
+		return $this->db_wrapper->getDB()->fetchAll();
+	}
+
 }
