fe7e0066b99f4f506a7cc2d118cf6f88473fbd4b
pyliu <3634168+pyliu@users.noreply.github.com>
2023-09-05 15:05:27 +0800

    clean code

--- www/debug.php
+++ www/debug.php
@@ -3,9 +3,7 @@ require_once("./include/init.php");
 require_once("./include/Prefetch.class.php");
 
 try {
-    // echo 'TEST wipeExpiredData <br/>';
-    // $result = Prefetch::wipeExpiredData();
-    // var_dump($result);
+    
 } catch(Exception $e) {
     die($e->getMessage());
 }
