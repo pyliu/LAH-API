25e28e55fae9e571c5dfccc186b9ad0e8ce212a5
pyliu <3634168+pyliu@users.noreply.github.com>
2024-02-29 08:55:12 +0800

    add more search type

--- www/include/MOIADM.class.php
+++ www/include/MOIADM.class.php
@@ -15,7 +15,7 @@ class MOIADM {
 		$this->db_wrapper = null;
 	}
 	/**
-	 * Find cert seq records
+	 * Find SMSLog records
 	 */
 	public function getSMSLogRecords($keyword) {
 		if (!$this->db_wrapper->reachable()) {
@@ -25,10 +25,64 @@ class MOIADM {
 			-- SMS Log 查詢
 			select * from MOIADM.SMSLOG t
 			where 1=1
-				and (ms14 like '%' || :bv_keyword || '%' OR MS_MAIL like '%' || :bv_keyword || '%')
+				and (ms14 like '%' || :bv_keyword || '%' OR MS_MAIL like '%' || :bv_keyword || '%' OR MS07_1 like '%' || :bv_keyword || '%' OR MS_NOTE like '%' || :bv_keyword || '%')
 			order by ms07_1 desc, ms07_2 desc
 		");
 		$this->db_wrapper->getDB()->bind(":bv_keyword", $keyword);
+		$this->db_wrapper->getDB()->execute();
+		return $this->db_wrapper->getDB()->fetchAll();
+	}
+	/**
+	 * Find SMSLog records by date
+	 */
+	public function getSMSLogRecordsByDate($date) {
+		if (!$this->db_wrapper->reachable()) {
+			return array();
+		}
+		$this->db_wrapper->getDB()->parse("
+			-- SMS Log 查詢
+			select * from MOIADM.SMSLOG t
+			where 1=1
+				and (MS07_1 like '%' || :bv_keyword || '%')
+			order by ms07_1 desc, ms07_2 desc
+		");
+		$this->db_wrapper->getDB()->bind(":bv_keyword", $date);
+		$this->db_wrapper->getDB()->execute();
+		return $this->db_wrapper->getDB()->fetchAll();
+	}
+	/**
+	 * Find SMSLog records by cell phone
+	 */
+	public function getSMSLogRecordsByCell($cell) {
+		if (!$this->db_wrapper->reachable()) {
+			return array();
+		}
+		$this->db_wrapper->getDB()->parse("
+			-- SMS Log 查詢
+			select * from MOIADM.SMSLOG t
+			where 1=1
+				and (MS14 like '%' || :bv_keyword || '%')
+			order by ms07_1 desc, ms07_2 desc
+		");
+		$this->db_wrapper->getDB()->bind(":bv_keyword", $cell);
+		$this->db_wrapper->getDB()->execute();
+		return $this->db_wrapper->getDB()->fetchAll();
+	}
+	/**
+	 * Find SMSLog records by email
+	 */
+	public function getSMSLogRecordsByEmail($mail) {
+		if (!$this->db_wrapper->reachable()) {
+			return array();
+		}
+		$this->db_wrapper->getDB()->parse("
+			-- SMS Log 查詢
+			select * from MOIADM.SMSLOG t
+			where 1=1
+				and (MS_MAIL like '%' || :bv_keyword || '%')
+			order by ms07_1 desc, ms07_2 desc
+		");
+		$this->db_wrapper->getDB()->bind(":bv_keyword", $mail);
 		$this->db_wrapper->getDB()->execute();
 		return $this->db_wrapper->getDB()->fetchAll();
 	}
