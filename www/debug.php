d50819ec0a471e506fc9eb975c51ec5532048936
pyliu <3634168+pyliu@users.noreply.github.com>
2025-06-12 09:12:11 +0800

    test

--- www/debug.php
+++ www/debug.php
@@ -1,7 +1,5 @@
 ﻿<?php
 require_once("./include/init.php");
-require_once(ROOT_DIR."/include/SQLiteAdminActionLog.class.php");
-require_once(ROOT_DIR."/include/MOISMS.class.php");
 require 'vendor/autoload.php';
 
 try {
@@ -10,7 +8,7 @@ try {
     // var_dump($records);
 
     // $moisms = new MOISMS();
-    // $result = $moisms->manualSendSMS('0911XXXXXX', '手動測試');
+    // $result = $moisms->manualSendSMS('0911225023', 'PHP手動測試');
     // $result = $moisms->manualSendBookingSMS('0911XXXXXX', 'BOOKING測試', '1140408', '083000');
     // $result = $moisms->isMA5NoExists('1140408000003');
     // $result = $moisms->setTodayMA05SMSToResend('0911XXXXXX', '手動測試');
